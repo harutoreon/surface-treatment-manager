ba0e45246fa59e11b5a6241ef8963b7a45c05e99
harutoreon <norabder567@gmail.com>
2023-11-11 20:16:39 +0900

    fix: Do assets:precompile

--- Rakefile
+++ Rakefile
@@ -1,17 +1,14 @@
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
-# require_relative "config/application"
+require_relative "config/application"
 
-# Rails.application.load_tasks
+Rails.application.load_tasks
 
-task(:default).clear
-task default: [:rubocop, :spec]
+if Rails.env.development?
+  require 'rubocop/rake_task'
+  RuboCop::RakeTask.new
 
-task :rubocop do
-  sh 'bundle exec rubocop'
-end
-
-task :spec do
-  sh 'bundle exec rspec'
+  task(:default).clear
+  task default: [:rubocop, :spec]
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
   # Do not eager load code on boot.
@@ -73,4 +68,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.assets.prefix = "/dev-assets"
 end
