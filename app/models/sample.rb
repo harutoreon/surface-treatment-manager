b87e2236ecf13af5ea59870e1cd1c4068886e5ef
harutoreon <norabder567@gmail.com>
2023-11-14 20:34:15 +0900

    add: Add validation

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,2 +1,6 @@
 class Sample < ApplicationRecord
+  validates :name,     presence: true
+  validates :category, presence: true
+  validates :color,    presence: true
+  validates :maker,    presence: true
 end
