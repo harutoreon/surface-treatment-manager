587fb2ebd8dc7e7b70445290a525eb3a691845f7
harutoreon <norabder567@gmail.com>
2023-11-16 05:08:03 +0900

    test: Check response

--- spec/requests/samples_spec.rb
+++ spec/requests/samples_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe "Samples", type: :request do
       expect(response.body).to include "Sample Show"
     end
   end
+
+  describe '#new' do
+    it 'レスポンスが正常であること' do
+      get new_sample_path
+      expect(response).to have_http_status :ok
+    end
+
+    it '見出しが表示されること' do
+      get new_sample_path
+      expect(response.body).to include "Sample New"
+    end
+  end
 end
