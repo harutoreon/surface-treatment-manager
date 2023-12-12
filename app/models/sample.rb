ff474338d59962bf8d53e3a41f0def5402922173
harutoreon <norabder567@gmail.com>
2023-12-13 04:09:07 +0900

    add: Limit the file size of images

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -4,4 +4,13 @@ class Sample < ApplicationRecord
   validates :color,    presence: true
   validates :maker,    presence: true
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
