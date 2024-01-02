1222339431193602b7809e0746506125ce7d02a3
harutoreon <norabder567@gmail.com>
2024-01-02 19:10:06 +0900

    clean: Change from Sample.search to scope

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -6,17 +6,26 @@ class Sample < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validate :picture_size
 
-  def self.search(search, word)
-    case search
-    when 'perfect'
-      Sample.where('name LIKE?', "#{word}")
-    when 'forward'
-      Sample.where('name LIKE?', "#{word}%")
-    when 'backward'
-      Sample.where('name LIKE?', "%#{word}")
-    when 'partial'
-      Sample.where('name LIKE?', "%#{word}%")
-    end
+  # def self.search(search, word)
+  #   case search
+  #   when 'perfect'
+  #     Sample.where('name LIKE?', "#{word}")
+  #   when 'forward'
+  #     Sample.where('name LIKE?', "#{word}%")
+  #   when 'backward'
+  #     Sample.where('name LIKE?', "%#{word}")
+  #   when 'partial'
+  #     Sample.where('name LIKE?', "%#{word}%")
+  #   end
+  # end
+
+  scope :search, -> (search, word) do
+    scope :perfect_search,  -> { where('name LIKE ?', "#{word}") }
+    scope :forward_search,  -> { where('name LIKE ?', "#{word}%") }
+    scope :backward_search, -> { where('name LIKE ?', "%#{word}") }
+    scope :partial_search,  -> { where('name LIKE ?', "%#{word}%") }
+
+    send("#{search}_search")
   end
 
   private
