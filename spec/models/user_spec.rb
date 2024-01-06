0930128770be50e8f62cf7300c4cc4e7072661c3
harutoreon <norabder567@gmail.com>
2024-01-07 06:02:07 +0900

    add: Add validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+
+  describe 'validation' do
+    it 'userが有効であること' do
+      expect(user).to be_valid
+    end
+
+    it 'nameが存在すること' do
+      user.name = ' '
+      expect(user).to_not be_valid
+    end
+
+    it 'nameは50文字以下であること' do
+      user.name = 's' * 51
+      expect(user).to_not be_valid
+    end
+  end
 end
