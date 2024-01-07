72936ab7dbb20f278cc2e58e0519b3fa13c832b8
harutoreon <norabder567@gmail.com>
2024-01-07 20:37:40 +0900

    add: Create user signup page and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name,     presence: true, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :name,                  presence: true, length: { maximum: 50 }
+  validates :password,              presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   has_secure_password
 end
