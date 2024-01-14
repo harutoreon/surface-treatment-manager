75609a40b89e24bbc331fce0653beecd7f482e0e
harutoreon <norabder567@gmail.com>
2024-01-15 04:31:06 +0900

    add: Add layout test

--- spec/system/user_login_spec.rb
+++ spec/system/user_login_spec.rb
@@ -5,12 +5,29 @@ RSpec.describe "UserLogin", type: :system do
     driven_by(:rack_test)
   end
 
-  context '無効な値の場合' do
-    it 'flashメッセージの残留がないこと' do
+  context '有効な値の場合' do
+    let(:user) { FactoryBot.create(:michael) }
+
+    it 'ログインできること' do
       visit login_path
+
+      fill_in 'Name',     with: user.name
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+
+      expect(page).to have_selector "a[href=\"#{samples_path}\"]"
+      expect(page).to have_selector "a[href=\"#{new_sample_path}\"]"
+    end
+  end
+
+  context '無効な値の場合' do
+    it 'ログインできないこと' do
+      visit login_path
+
       fill_in 'Name',     with: ''
       fill_in 'Password', with: ''
       click_button 'Log in'
+
       expect(page).to have_selector 'div.alert.alert-danger'
 
       visit login_path
