5a49ff43988cf49b67d2cce2449ea414b45a73bc
harutoreon <norabder567@gmail.com>
2024-01-14 10:25:11 +0900

    add: Provisional implementation create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
