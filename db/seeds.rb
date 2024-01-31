5bfbffe9f9d7acb8287edeba2ebcef8d89995bbe
harutoreon <norabder567@gmail.com>
2024-02-01 03:00:14 +0900

    add: Add category plating

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,33 @@ User.create!(name: "Sample User", password: "foobaz", password_confirmation: "fo
 
 # 表面処理一覧
 SAMPLES = [
-  { name: "無電解ニッケルめっき", category: "めっき", color: "飴色のシルバー", maker: Faker::Company.name },
+  { name: "無電解ニッケルめっき", category: "めっき", color: "イエローブラウンシルバー", maker: Faker::Company.name },
   { name: "白銀めっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
   { name: "金めっき", category: "めっき", color: "ゴールド", maker: Faker::Company.name },
   { name: "銀めっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
-  { name: "銅めっき", category: "めっき", color: "ブロンズ", maker: Faker::Company.name }
+  { name: "銅めっき", category: "めっき", color: "ブロンズ", maker: Faker::Company.name },
+  { name: "亜鉛めっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
+  { name: "錫めっき", category: "めっき", color: "ホワイトシルバー", maker: Faker::Company.name },
+  { name: "ニッケルめっき", category: "めっき", color: "ライトシルバー", maker: Faker::Company.name },
+  { name: "クロムめっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
+  { name: "黒色クロムめっき", category: "めっき", color: "マットブラック", maker: Faker::Company.name }
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
+  # { name: "", category: "", color: "", maker: Faker::Company.name },
 ]
 
 picture = File.open("app/assets/images/kitten.jpg")
