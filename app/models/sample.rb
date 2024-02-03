850209cd95b909c3122f8fca00871c88ddcc8dec
harutoreon <norabder567@gmail.com>
2024-02-04 03:34:59 +0900

    add: Add validation presence to picture

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -3,28 +3,15 @@ class Sample < ApplicationRecord
   validates :category, presence: true
   validates :color,    presence: true
   validates :maker,    presence: true
+  validates :picture,  presence: true, allow_nil: true
+  validate  :picture_size
   mount_uploader :picture, PictureUploader
-  validate :picture_size
-
-  # def self.search(search, word)
-  #   case search
-  #   when 'perfect'
-  #     Sample.where('name LIKE?', "#{word}")
-  #   when 'forward'
-  #     Sample.where('name LIKE?', "#{word}%")
-  #   when 'backward'
-  #     Sample.where('name LIKE?', "%#{word}")
-  #   when 'partial'
-  #     Sample.where('name LIKE?', "%#{word}%")
-  #   end
-  # end
 
   scope :search, -> (search, word) do
     scope :perfect_search,  -> { where('name LIKE ?', "#{word}") }
     scope :forward_search,  -> { where('name LIKE ?', "#{word}%") }
     scope :backward_search, -> { where('name LIKE ?', "%#{word}") }
     scope :partial_search,  -> { where('name LIKE ?', "%#{word}%") }
-
     send("#{search}_search")
   end
 
