feb492c6cc4df163a13a46db8df2b99b9a4a27a5
harutoreon <norabder567@gmail.com>
2024-02-04 02:45:35 +0900

    change: Image replacement completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,47 @@
 Faker::Config.locale = 'ja'
 
-# ユーザー
 User.create!(name: "Example User", password: "foobar", password_confirmation: "foobar", admin: true)
 User.create!(name: "Sample User", password: "foobaz", password_confirmation: "foobaz")
 
-# 表面処理一覧
 SAMPLES = [
-  { name: "無電解ニッケルめっき", category: "めっき", color: "イエローブラウンシルバー", maker: Faker::Company.name },
-  { name: "白銀めっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
-  { name: "金めっき", category: "めっき", color: "ゴールド", maker: Faker::Company.name },
-  { name: "銀めっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
-  { name: "銅めっき", category: "めっき", color: "ブロンズ", maker: Faker::Company.name },
-  { name: "亜鉛めっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
-  { name: "錫めっき", category: "めっき", color: "ホワイトシルバー", maker: Faker::Company.name },
-  { name: "ニッケルめっき", category: "めっき", color: "ライトシルバー", maker: Faker::Company.name },
-  { name: "クロムめっき", category: "めっき", color: "シルバー", maker: Faker::Company.name },
-  { name: "黒色クロムめっき", category: "めっき", color: "マットブラック", maker: Faker::Company.name },
-  { name: "白アルマイト", category: "陽極酸化", color: "ホワイト", maker: Faker::Company.name },
-  { name: "黒アルマイト", category: "陽極酸化", color: "ブラック", maker: Faker::Company.name },
-  { name: "硬質アルマイト", category: "陽極酸化", color: "ダークブラウン", maker: Faker::Company.name },
-  { name: "ユニクロクロメート", category: "化成", color: "シルバー", maker: Faker::Company.name },
-  { name: "有色クロメート", category: "化成", color: "イエローブラウン", maker: Faker::Company.name },
-  { name: "黒クロメート", category: "化成", color: "ブラック", maker: Faker::Company.name },
-  { name: "緑クロメート", category: "化成", color: "オリーブ", maker: Faker::Company.name },
-  { name: "四三酸化鉄皮膜", category: "化成", color: "ブラック", maker: Faker::Company.name },
-  { name: "パーカー", category: "化成", color: "ブラック", maker: Faker::Company.name },
-  { name: "TiN", category: "コーティング", color: "ゴールド", maker: Faker::Company.name },
-  { name: "TiC", category: "コーティング", color: "シルバー", maker: Faker::Company.name },
-  { name: "TiCN", category: "コーティング", color: "グレー", maker: Faker::Company.name },
-  { name: "TiAlN", category: "コーティング", color: "ダークバイオレット", maker: Faker::Company.name },
-  { name: "AlCrN", category: "コーティング", color: "ダークグレー", maker: Faker::Company.name },
-  { name: "CrN", category: "コーティング", color: "シルバーグレー", maker: Faker::Company.name },
-  { name: "DLC", category: "コーティング", color: "ブラック", maker: Faker::Company.name },
-  { name: "ブラスト", category: "表面硬化", color: "メタリックグレー", maker: Faker::Company.name },
-  { name: "WPC", category: "表面硬化", color: "グレー", maker: Faker::Company.name },
-  { name: "レイデント", category: "めっき", color: "ブラック", maker: Faker::Company.name },
-  { name: "パルソナイト", category: "表面硬化", color: "グレー", maker: Faker::Company.name },
-  { name: "タフトライド", category: "表面硬化", color: "グレー", maker: Faker::Company.name },
-  { name: "キリンコートS", category: "表面硬化", color: "メタリックグレー", maker: Faker::Company.name },
-  { name: "カナック", category: "表面硬化", color: "ブラウン", maker: Faker::Company.name }
+  { name: "無電解ニッケルめっき", category: "めっき", color: "イエローブラウンシルバー", picture: "electroless_nickel_plating" },
+  { name: "白銀めっき", category: "めっき", color: "シルバー", picture: "white_silver_plating" },
+  { name: "金めっき", category: "めっき", color: "ゴールド", picture: "gold_plate" },
+  { name: "銀めっき", category: "めっき", color: "シルバー", picture: "silver_plating" },
+  { name: "銅めっき", category: "めっき", color: "ブロンズ", picture: "copper_plating" },
+  { name: "亜鉛めっき", category: "めっき", color: "シルバー", picture: "zinc_plating" },
+  { name: "錫めっき", category: "めっき", color: "ホワイトシルバー", picture: "tin_plating" },
+  { name: "ニッケルめっき", category: "めっき", color: "ライトシルバー", picture: "nickel_plating" },
+  { name: "クロムめっき", category: "めっき", color: "シルバー", picture: "chrome_plating" },
+  { name: "黒色クロムめっき", category: "めっき", color: "マットブラック", picture: "black_chrome_plating" },
+  { name: "白アルマイト", category: "陽極酸化", color: "ホワイト", picture: "white_anodized_aluminum" },
+  { name: "黒アルマイト", category: "陽極酸化", color: "ブラック", picture: "black_anodized_aluminum" },
+  { name: "硬質アルマイト", category: "陽極酸化", color: "ダークブラウン", picture: "hard_anodized_aluminum" },
+  { name: "ユニクロクロメート", category: "化成", color: "シルバー", picture: "unichromate" },
+  { name: "有色クロメート", category: "化成", color: "イエローブラウン", picture: "colored_chromate" },
+  { name: "黒クロメート", category: "化成", color: "ブラック", picture: "black_chromate" },
+  { name: "緑クロメート", category: "化成", color: "オリーブ", picture: "green_chromate" },
+  { name: "四三酸化鉄皮膜", category: "化成", color: "ブラック", picture: "iron_tetroxide_film" },
+  { name: "パーカー", category: "化成", color: "ブラック", picture: "hooded sweatshirt" },
+  { name: "TiN", category: "コーティング", color: "ゴールド", picture: "titanium_coating" },
+  { name: "TiCN", category: "コーティング", color: "グレー", picture: "titanium_ceramic_coating" },
+  { name: "TiAlN", category: "コーティング", color: "ダークバイオレット", picture: "titanium_aluminum_coating" },
+  { name: "AlCrN", category: "コーティング", color: "ダークグレー", picture: "aluminum_chrome_coating" },
+  { name: "CrN", category: "コーティング", color: "シルバーグレー", picture: "chromium_nitride_coating" },
+  { name: "DLC", category: "コーティング", color: "ブラック", picture: "diamond_like_carbon" },
+  { name: "ブラスト", category: "表面硬化", color: "メタリックグレー", picture: "blast" },
+  { name: "WPC", category: "表面硬化", color: "グレー", picture: "wonder_process_craft" },
+  { name: "レイデント", category: "めっき", color: "ブラック", picture: "raident" },
+  { name: "パルソナイト", category: "表面硬化", color: "グレー", picture: "palsonite" },
+  { name: "タフトライド", category: "表面硬化", color: "グレー", picture: "tufted_ride" },
+  { name: "キリンコートS", category: "表面硬化", color: "メタリックグレー", picture: "kirin_coat_s" },
+  { name: "カナック", category: "表面硬化", color: "ブラウン", picture: "canac" }
 ]
 
-picture = File.open("app/assets/images/kitten.jpg")
-
 SAMPLES.each do |sample|
-  Sample.create!(name: sample[:name], category: sample[:category], color: sample[:color], maker: sample[:maker], picture: picture)
+  Sample.create!(name: sample[:name],
+                 category: sample[:category],
+                 color: sample[:color],
+                 maker: Faker::Company.name,
+                 picture: File.open("app/assets/images/#{sample[:picture]}.jpeg"))
 end
