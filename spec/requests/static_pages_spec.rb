522ed20c43dc15975238b8cc344f0c38173dd110
harutoreon <norabder567@gmail.com>
2024-02-10 02:13:20 +0900

    add: Add action to static_pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  # describe "#home" do
-  #   it "レスポンスが正常であること" do
-  #     get root_path
-  #     expect(response).to have_http_status(:success)
-  #   end
+  describe "#home" do
+    it "レスポンスが正常であること" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  #   it 'Welcomeメッセージが表示されること' do
-  #     get root_path
-  #     expect(response.body).to include "Welcome to Surface Treatment Manager"
-  #   end
+  describe '#name' do
+    it 'レスポンスが正常であること' do
+      get name_path
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  #   it 'samples/indexのリンクが存在すること' do
-  #     get root_path
-  #     expect(response.body).to include "<a href=\"#{samples_path}\">"
-  #   end
-  # end
+  describe '#category' do
+    it 'レスポンスが正常であること' do
+      get category_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#maker' do
+    it 'レスポンスが正常であること' do
+      get maker_path
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
