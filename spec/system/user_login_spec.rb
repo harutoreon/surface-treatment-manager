0f0a80cb41027ee6c68a8bf35a4563739361ab60
harutoreon <norabder567@gmail.com>
2024-02-12 04:24:48 +0900

    change: Change redirect destination after login

--- spec/system/user_login_spec.rb
+++ spec/system/user_login_spec.rb
@@ -5,33 +5,36 @@ RSpec.describe "UserLogin", type: :system do
     driven_by(:rack_test)
   end
 
-  context '有効な値の場合' do
+  context '有効な値を入力した場合' do
     let(:user) { FactoryBot.create(:michael) }
 
-    it 'ログインできること' do
+    it 'static_pages#homeページにリダイレクトすること（ログイン成功）' do
       visit login_path
 
-      fill_in 'Name',     with: user.name
-      fill_in 'Password', with: user.password
-      click_button 'Log in'
+      fill_in('Name',     with: user.name)
+      fill_in('Password', with: user.password)
 
-      expect(page).to have_selector "a[href=\"#{samples_path}\"]"
-      expect(page).to have_selector "a[href=\"#{new_sample_path}\"]"
+      click_button('Log in')
+
+      expect(page).to have_content('Search Menu')
+      expect(page).to have_link('Go name search page', href: name_path)
+      expect(page).to have_link('Go category search page', href: category_path)
+      expect(page).to have_link('Go maker search page', href: maker_path)
     end
   end
 
-  context '無効な値の場合' do
-    it 'ログインできないこと' do
+  context '無効な値を入力した場合' do
+    it 'sessions#newページを再描画すること（ログイン失敗）' do
       visit login_path
 
-      fill_in 'Name',     with: ''
-      fill_in 'Password', with: ''
-      click_button 'Log in'
+      fill_in('Name',     with: '')
+      fill_in('Password', with: '')
 
-      expect(page).to have_selector 'div.alert.alert-danger'
+      click_button('Log in')
+      expect(page).to have_selector('div.alert.alert-danger')
 
       visit login_path
-      expect(page).to_not have_selector 'div.alert.alert-danger'
+      expect(page).to_not have_selector('div.alert.alert-danger')
     end
   end
 end
