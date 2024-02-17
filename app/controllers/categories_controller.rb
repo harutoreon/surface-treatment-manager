06565c2287e944343fe1b709045ed0e76576dd03
harutoreon <norabder567@gmail.com>
2024-02-17 21:35:43 +0900

    add: Add Categories#create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,20 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      # redirect_to @category
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:item)
+    end
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
+  before do
+    user = FactoryBot.create(:user)
+    log_in(user)
+
+    @sample = FactoryBot.create(:sample)
+  end
+
   describe "GET /index" do
     # pending "add some examples (or delete) #{__FILE__}"
   end
@@ -9,6 +16,28 @@ RSpec.describe "Categories", type: :request do
     it 'レスポンスが正常であること' do
       get new_category_path
       expect(response).to have_http_status :success
+    end
+  end
+
+  describe '#create' do
+    let(:valid_params) { { category: { item: "溶射" } } }
+    let(:invalid_params) { { category: { item: "" } } }
+
+    context '有効なパラメータの場合' do
+      it '登録が完了すること' do
+        expect { post categories_path, params: valid_params }.to change{ Category.count }.from(0).to(1)
+      end
+    end
+
+    context '無効なパラメータの場合' do
+      it '登録が失敗すること' do
+        expect { post categories_path, params: invalid_params }.to_not change{ Category.count }.from(0)
+      end
+
+      it 'categories/newに遷移すること' do
+        post categories_path, params: invalid_params
+        expect(response.body).to include("Category New")
+      end
     end
   end
 end
