94a1409b2e6a76be16b123a98ef92fbffa2e1d65
harutoreon <norabder567@gmail.com>
2024-02-17 21:03:28 +0900

    add: Add Categories#new

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe "Categories", type: :request do
   describe "GET /index" do
     # pending "add some examples (or delete) #{__FILE__}"
   end
+
+  describe '#new' do
+    it 'レスポンスが正常であること' do
+      get new_category_path
+      expect(response).to have_http_status :success
+    end
+  end
 end
