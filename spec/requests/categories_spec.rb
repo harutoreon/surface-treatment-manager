396e0e4f4116113fc13fc0237ce63b8cd643edf1
harutoreon <norabder567@gmail.com>
2024-02-20 20:37:21 +0900

    add: Add delete method test

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -98,5 +98,10 @@ RSpec.describe "Categories", type: :request do
     it '削除できること' do
       expect { delete category_path(@category) }.to change{ Category.count }.from(1).to(0)
     end
+
+    it 'categories#indexにリダイレクトされうること' do
+      delete category_path(@category)
+      expect(response).to redirect_to(categories_url)
+    end
   end
 end
