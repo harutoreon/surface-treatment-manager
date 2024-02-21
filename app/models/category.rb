931b07fdd008c3b769643f6a81c5ceb4c1e1e435
harutoreon <norabder567@gmail.com>
2024-02-22 02:01:42 +0900

    add: Add validation test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  validates :item, presence: true
+  validates :item, presence: true, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:category) { FactoryBot.create(:category) }
+
+    it 'カテゴリが有効であること' do
+      expect(category).to be_valid
+    end
+
+    it 'itemが存在すること' do
+      category.item = ''
+      expect(category).to_not be_valid
+    end
+
+    it 'itemの重複がないこと' do
+      duplicate_category = category.dup
+      expect(duplicate_category).to_not be_valid
+    end
+  end
 end
