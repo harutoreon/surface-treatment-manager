1f5f47fde25768460897908d61c78a467b3a54f1
harutoreon <norabder567@gmail.com>
2024-03-01 01:50:10 +0900

    add: Add #current_user

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -40,4 +40,21 @@ RSpec.describe SessionsHelper, type: :helper do
       end
     end
   end
+
+  describe '#current_user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    context 'ログインしている場合' do
+      it 'ユーザーが返ること' do
+        log_in(user)
+        expect(current_user).to eq(user)
+      end
+    end
+
+    context '未ログインの場合' do
+      it 'nilが返ること' do
+        expect(current_user).to be_nil
+      end
+    end
+  end
 end
