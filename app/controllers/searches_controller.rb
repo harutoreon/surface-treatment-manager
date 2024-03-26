a53bc513706dd9c3fb1717be07a281666315ebe4
harutoreon <norabder567@gmail.com>
2024-03-27 02:56:09 +0900

    Update category search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,7 @@ class SearchesController < ApplicationController
 
   def category_search
     @samples = Sample.category_search(params[:selectword]).paginate(page: params[:page], per_page: 8)
+    @selectword = params[:selectword]
   end
 
   def maker_search
