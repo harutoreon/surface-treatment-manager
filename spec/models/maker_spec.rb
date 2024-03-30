b8bd8579002f2bc614325f9e1fd6895e9c6eac1d
harutoreon <norabder567@gmail.com>
2024-03-30 19:42:17 +0900

    Add validation test

--- spec/models/maker_spec.rb
+++ spec/models/maker_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Maker, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    before do
+      @maker = FactoryBot.create(:maker)
+    end
+
+    it 'makerが有効であること' do
+      expect(@maker).to be_valid
+    end
+
+    it 'nameが存在すること' do
+      @maker.name = ''
+      expect(@maker).to_not be_valid
+    end
+
+    it 'postal_codeが存在すること' do
+      @maker.postal_code = ''
+      expect(@maker).to_not be_valid
+    end
+
+    it 'addressが存在すること' do
+      @maker.address = ''
+      expect(@maker).to_not be_valid
+    end
+
+    it 'phone_numberが存在すること' do
+      @maker.phone_number = ''
+      expect(@maker).to_not be_valid
+    end
+
+    it 'fax_numberが存在すること' do
+      @maker.fax_number = ''
+      expect(@maker).to_not be_valid
+    end
+
+    it 'emailが存在すること' do
+      @maker.email = ''
+      expect(@maker).to_not be_valid
+    end
+
+    it 'home_pageが存在すること' do
+      @maker.home_page = ''
+      expect(@maker).to_not be_valid
+    end
+
+    it 'manufacturer_repが存在すること' do
+      @maker.manufacturer_rep = ''
+      expect(@maker).to_not be_valid
+    end
+  end
 end
