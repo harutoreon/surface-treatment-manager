23d2c25b849a6e2ff0f15d4411379ec3acaf55a7
harutoreon <norabder567@gmail.com>
2024-04-03 01:42:30 +0900

    Partialize the message on the login page

--- spec/system/user_login_spec.rb
+++ spec/system/user_login_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "UserLogin", type: :system do
   context '有効な値を入力した場合' do
     let(:user) { FactoryBot.create(:michael) }
 
-    it 'static_pages#homeページにリダイレクトすること（ログイン成功）' do
+    it 'static_pages/homeにリダイレクトすること（ログイン成功）' do
       visit login_path
 
       fill_in('Name',     with: user.name)
@@ -17,23 +17,22 @@ RSpec.describe "UserLogin", type: :system do
       click_button('Log in')
 
       expect(page).to have_content('Main Menu')
-      expect(page).to have_link(id: 'name_search', href: category_name_path)
-      expect(page).to have_link(id: 'category_search', href: category_category_path)
-      expect(page).to have_link(id: 'maker_search', href: category_maker_path)
     end
   end
 
   context '無効な値を入力した場合' do
-    it 'sessions#newページを再描画すること（ログイン失敗）' do
+    it 'sessions/newを再描画すること（ログイン失敗）' do
       visit login_path
 
       fill_in('Name',     with: '')
       fill_in('Password', with: '')
 
       click_button('Log in')
+
       expect(page).to have_selector('div.alert.alert-danger')
 
       visit login_path
+
       expect(page).to_not have_selector('div.alert.alert-danger')
     end
   end
