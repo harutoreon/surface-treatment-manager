1da59a5f3aec22e19e52444d858526ede49d570b
harutoreon <norabder567@gmail.com>
2024-04-11 20:23:19 +0900

    Add field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_07_174552) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_11_111434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_07_174552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.string "hardness"
+    t.string "film_thickness"
+    t.string "feature"
   end
 
   create_table "users", force: :cascade do |t|
