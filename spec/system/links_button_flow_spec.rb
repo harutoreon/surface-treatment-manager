6035dcd05760b78636173bfae21b80e64fe68d6b
harutoreon <norabder567@gmail.com>
2024-04-19 04:45:41 +0900

    Add test

---
+++ spec/system/links_button_flow_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "LinksButtonFlow", type: :system do
+  before do
+    driven_by(:selenium)
+    @sample = FactoryBot.create(:sample)
+  end
+
+  describe 'samples/show', js: true do
+    context 'Linksボタンを押下しない時' do
+      it 'aria-expanded属性がfalseであること' do
+        visit sample_path(@sample)
+
+        expect(page).to have_selector('button[aria-expanded="false"]')
+      end
+    end
+    context 'Linksボタン押下した時' do
+      it 'aria-expanded属性がtrueであること' do
+        visit sample_path(@sample)
+
+        click_button('Links')
+
+        expect(page).to have_selector('button[aria-expanded="true"]')
+      end
+    end
+  end
+end
