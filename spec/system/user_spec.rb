61a833f26aea7dee9f58b474f1a4331322b58ef3
harutoreon <norabder567@gmail.com>
2024-04-29 01:03:17 +0900

    Delete unnecessary tests

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,16 +1,16 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
-  describe '#show' do
-    let!(:admin)      { FactoryBot.create(:user) }
-    let!(:not_admin)  { FactoryBot.create(:archer) }
-    let!(:other_user) { FactoryBot.create(:michael) }
+# RSpec.describe "Users", type: :system do
+#   describe '#show' do
+#     let!(:admin)      { FactoryBot.create(:user) }
+#     let!(:not_admin)  { FactoryBot.create(:archer) }
+#     let!(:other_user) { FactoryBot.create(:michael) }
 
-    # it 'ログインユーザーが管理者なら削除リンクが表示されること' do
-    #   log_in(admin)
-    #   visit user_path(other_user)
+#     it 'ログインユーザーが管理者なら削除リンクが表示されること' do
+#       log_in(admin)
+#       visit user_path(other_user)
 
-    #   expect(page).to have_link("User Information")
-    # end
-  end
-end
+#       expect(page).to have_link("User Information")
+#     end
+#   end
+# end
