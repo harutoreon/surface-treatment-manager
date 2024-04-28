de07d971a801291d73a8a514c1ef5c2f0295147c
harutoreon <norabder567@gmail.com>
2024-04-29 01:00:14 +0900

    Remove duplicate codes

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
   describe '#show' do
     let!(:admin)      { FactoryBot.create(:user) }
     let!(:not_admin)  { FactoryBot.create(:archer) }
