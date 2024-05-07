daddad715f56e11ef6207ca3616047b69c6bab0c
harutoreon <norabder567@gmail.com>
2024-05-08 03:04:15 +0900

    Add comment display function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @sample = Sample.find(params[:sample_id])
+    @comment = @sample.comments.create(comment_params)
+    redirect_to sample_path(@sample)
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
