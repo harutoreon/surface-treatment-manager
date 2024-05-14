9aa61505dfab64d51f74cf5a3e9b62ead00e095c
harutoreon <norabder567@gmail.com>
2024-05-15 04:18:23 +0900

    Move logged_in_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,15 +50,4 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
     end
-
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
-    def admin_user
-      redirect_to(login_url) unless current_user.admin?
-    end
 end
