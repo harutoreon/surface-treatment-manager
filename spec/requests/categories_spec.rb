08fd3a3da6cc7e480a078f3d009b01abbb86d2be
harutoreon <norabder567@gmail.com>
2024-05-16 05:00:02 +0900

    Modify Categories controller

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,24 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  before do
-    # admin = FactoryBot.create(:admin)
-    # log_in(admin)
-
-    @category = FactoryBot.create(:category)
-  end
-
   describe "#index" do
     it 'レスポンスが正常であること' do
       get categories_path
       expect(response).to have_http_status(:success)
     end
-
     it '見出しが表示されること' do
       get categories_path
       expect(response.body).to include('Category List')
     end
-
     it 'タイトルが表示されること' do
       get categories_path
       expect(response.body).to include('<title>Category List</title>')
@@ -26,16 +17,18 @@ RSpec.describe "Categories", type: :request do
   end
 
   describe '#show' do
+    before do
+      @category = FactoryBot.create(:category)
+    end
+
     it 'レスポンスが正常であること' do
       get category_path(@category)
       expect(response).to have_http_status(:success)
     end
-
     it '見出しが表示されること' do
       get category_path(@category)
       expect(response.body).to include('Category Information')
     end
-
     it 'タイトルが表示されること' do
       get category_path(@category)
       expect(response.body).to include('<title>Category Information</title>')
@@ -47,12 +40,10 @@ RSpec.describe "Categories", type: :request do
       get new_category_path
       expect(response).to have_http_status(:success)
     end
-
     it '見出しが表示されること' do
       get new_category_path
       expect(response.body).to include('New Registration for Category')
     end
-
     it 'タイトルが表示されること' do
       get new_category_path
       expect(response.body).to include('<title>New Registration for Category</title>')
@@ -64,22 +55,19 @@ RSpec.describe "Categories", type: :request do
     let(:invalid_params) { { category: { item: "", summary: '溶射金属やセラミックスなどの粉末を、溶解状態にして製品表面に吹き付ける処理のこと。' } } }
 
     context '有効なパラメータの場合' do
-      it '登録が成功すること' do
-        expect { post categories_path, params: valid_params }.to change{ Category.count }.from(1).to(2)
+      it 'レコードが1件増加すること' do
+        expect { post categories_path, params: valid_params }.to change{ Category.count }.from(0).to(1)
       end
-
       it 'categories#showにリダイレクトされること' do
         post categories_path, params: valid_params
         category = Category.last
         expect(response).to redirect_to(category)
       end
     end
-
     context '無効なパラメータの場合' do
-      it '登録が失敗すること' do
-        expect { post categories_path, params: invalid_params }.to_not change{ Category.count }.from(1)
+      it 'レコード数が変わらないこと' do
+        expect { post categories_path, params: invalid_params }.to_not change{ Category.count }.from(0)
       end
-
       it 'categories/newに遷移すること' do
         post categories_path, params: invalid_params
         expect(response.body).to include("New Registration for Category")
@@ -88,60 +76,117 @@ RSpec.describe "Categories", type: :request do
   end
 
   describe '#edit' do
-    it 'レスポンスが正常であること' do
-      get edit_category_path(@category)
-      expect(response).to have_http_status(:success)
+    before do
+      @category = FactoryBot.create(:category)
     end
 
-    it '見出しが「Category Edit」であること' do
-      get edit_category_path(@category)
-      expect(response.body).to include('Edit for Category')
-    end
+    context 'ログイン済みの場合' do
+      before do
+        user = FactoryBot.create(:user)
+        log_in(user)
+      end
 
-    it 'タイトルが表示されること' do
-      get edit_category_path(@category)
-      expect(response.body).to include('<title>Edit for Category</title>')
+      it 'レスポンスが正常であること' do
+        get edit_category_path(@category)
+        expect(response).to have_http_status(:success)
+      end
+      it '見出しが「Category Edit」であること' do
+        get edit_category_path(@category)
+        expect(response.body).to include('Edit for Category')
+      end
+      it 'タイトルが表示されること' do
+        get edit_category_path(@category)
+        expect(response.body).to include('<title>Edit for Category</title>')
+      end
+    end
+    context '未ログインの場合' do
+      it "ログインページにリダイレクトされること" do
+        get edit_category_path(@category)
+        assert_redirected_to login_url
+      end
+      it 'フラッシュメッセージが表示されること' do
+        get edit_category_path(@category)
+        expect(flash[:danger]).to eq('Please log in.')
+      end
     end
   end
 
   describe '#update' do
-    context '有効なパラメータの場合' do
-      it '更新が成功すること' do
-        patch category_path(@category), params: { category: { item: '陽極酸化' } }
-        @category.reload
-        expect(@category.item).to eq('陽極酸化')
-      end
-
-      it 'categories#showにリダイレクトされること' do
-        patch category_path(@category), params: { category: { item: '陽極酸化' } }
-        @category.reload
-        expect(response).to redirect_to(@category)
-      end
+    before do
+      @category = FactoryBot.create(:category)
     end
 
-    context '無効なパラメータの場合' do
-      it '更新が失敗すること' do
-        patch category_path(@category), params: { category: { item: '' } }
-        @category.reload
-        expect(@category.item).to eq('めっき')
+    context 'ログイン済みで' do
+      before do
+        user = FactoryBot.create(:user)
+        log_in(user)
       end
 
-      it 'categories#editに遷移されること' do
-        patch category_path(@category), params: { category: { item: '' } }
-        @category.reload
-        expect(response.body).to include("Edit for Category")
+      context '有効なパラメータの場合' do
+        it '更新が成功すること' do
+          patch category_path(@category), params: { category: { item: '陽極酸化' } }
+          @category.reload
+          expect(@category.item).to eq('陽極酸化')
+        end
+        it 'categories#showにリダイレクトされること' do
+          patch category_path(@category), params: { category: { item: '陽極酸化' } }
+          @category.reload
+          expect(response).to redirect_to(@category)
+        end
+      end
+      context '無効なパラメータの場合' do
+        it '更新が失敗すること' do
+          patch category_path(@category), params: { category: { item: '' } }
+          @category.reload
+          expect(@category.item).to eq('めっき')
+        end
+        it 'categories#editに遷移されること' do
+          patch category_path(@category), params: { category: { item: '' } }
+          @category.reload
+          expect(response.body).to include("Edit for Category")
+        end
+      end
+    end
+    context '未ログインの場合' do
+      it "ログインページにリダイレクトされること" do
+        patch category_path(@category), params: { category: { item: '陽極酸化' } }
+        assert_redirected_to login_url
+      end
+      it 'フラッシュメッセージが表示されること' do
+        patch category_path(@category), params: { category: { item: '陽極酸化' } }
+        expect(flash[:danger]).to eq('Please log in.')
       end
     end
   end
 
-  describe 'destroy' do
-    it '削除できること' do
-      expect { delete category_path(@category) }.to change{ Category.count }.from(1).to(0)
+  describe '#destroy' do
+    before do
+      @category = FactoryBot.create(:category)
     end
 
-    it 'categories#indexにリダイレクトされること' do
-      delete category_path(@category)
-      expect(response).to redirect_to(categories_url)
+    context 'ログイン済みの場合' do
+      before do
+        user = FactoryBot.create(:user)
+        log_in(user)
+      end
+
+      it 'レコードが1件減少すること' do
+        expect { delete category_path(@category) }.to change{ Category.count }.from(1).to(0)
+      end
+      it 'categories#indexにリダイレクトされること' do
+        delete category_path(@category)
+        expect(response).to redirect_to(categories_url)
+      end
+    end
+    context '未ログインの場合' do
+      it "ログインページにリダイレクトされること" do
+        delete category_path(@category)
+        assert_redirected_to login_url
+      end
+      it 'フラッシュメッセージが表示されること' do
+        delete category_path(@category)
+        expect(flash[:danger]).to eq('Please log in.')
+      end
     end
   end
 end
--- spec/system/categories_management_flow_spec.rb
+++ spec/system/categories_management_flow_spec.rb
@@ -2,79 +2,123 @@ require 'rails_helper'
 
 RSpec.describe "CategoriesManagementFlow", type: :system do
   before do
-    # user = FactoryBot.create(:user)
-    # log_in(user)
-
     @category = FactoryBot.create(:category)
   end
 
   describe 'categories#create' do
     context '有効な値を入力した場合' do
-      it '登録に成功すること' do
+      it 'categories/showページが表示されること' do
         visit new_category_path
-
         fill_in('Item',    with: '溶射')
         fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
-
         click_button('Create Category')
-
-        expect(page).to have_content('Category Information')
-        expect(page).to have_selector('div.alert.alert-success')
+        expect(page).to have_selector('h3', text: 'Category Information')
+      end
+      it 'フラッシュメッセージが表示されること' do
+        visit new_category_path
+        fill_in('Item',    with: '溶射')
+        fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
+        click_button('Create Category')
+        expect(page).to have_selector('div', text: 'Successful registration of new category!')
       end
     end
-
     context '無効な値を入力した場合' do
-      it '登録に失敗すること' do
+      it 'categories/newページが表示されること' do
         visit new_category_path
-
         fill_in('Item',    with: '')
         fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
-
         click_button('Create Category')
-
-        expect(page).to have_content('New Registration for Category')
-        expect(page).to have_content('Item can\'t be blank')
+        expect(page).to have_selector('h3', text: 'New Registration for Category')
+      end
+      it 'バリデーションエラーが表示されること' do
+        visit new_category_path
+        fill_in('Item',    with: '')
+        fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
+        click_button('Create Category')
+        expect(page).to have_selector('div', text: 'Item can\'t be blank')
       end
     end
   end
 
   describe 'categories#update' do
-    context '有効な値を入力した場合' do
-      it '更新できること' do
-        visit edit_category_path(@category)
+    context 'ログイン済みで' do
+      before do
+        user = FactoryBot.create(:user)
+        log_in(user)
+      end
 
-        fill_in('Item', with: '溶射')
-        fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
-
-        click_button('Update Category')
-
-        expect(page).to have_content('Category Information')
-        expect(page).to have_selector('div.alert.alert-success')
+      context '有効な値を入力した場合' do
+        it 'categories/showページが表示されること' do
+          visit edit_category_path(@category)
+          fill_in('Item', with: '溶射')
+          fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
+          click_button('Update Category')
+          expect(page).to have_selector('h3', text: 'Category Information')
+        end
+        it 'フラッシュメッセージが表示されること' do
+          visit edit_category_path(@category)
+          fill_in('Item', with: '溶射')
+          fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
+          click_button('Update Category')
+          expect(page).to have_selector('div', text: 'Successful updated category information!')
+        end
+      end
+      context '無効な値を入力した場合' do
+        it 'categories/editページが表示されること' do
+          visit edit_category_path(@category)
+          fill_in('Item', with: '')
+          click_button('Update Category')
+          expect(page).to have_selector('h3', text: 'Edit for Category')
+        end
+        it 'バリデーションエラーが出ること' do
+          visit edit_category_path(@category)
+          fill_in('Item', with: '')
+          click_button('Update Category')
+          expect(page).to have_selector('div', text: 'Item can\'t be blank')
+        end
       end
     end
-
-    context '無効な値を入力した場合' do
-      it '更新できないこと' do
+    context '未ログインの場合' do
+      it 'ログインページにリダイレクトされること' do
         visit edit_category_path(@category)
-
-        fill_in('Item', with: '')
-
-        click_button('Update Category')
-
-        expect(page).to have_content('Edit for Category')
-        expect(page).to have_content('Item can\'t be blank')
+        expect(page).to have_selector('h3', text: 'Log in')
+      end
+      it 'フラッシュメッセージが表示されること' do
+        visit edit_category_path(@category)
+        expect(page).to have_selector('div', text: 'Please log in.')
       end
     end
   end
 
   describe 'categories#destory' do
-    it '削除できること' do
-      visit category_path(@category)
+    context 'ログイン済みの場合' do
+      before do
+        user = FactoryBot.create(:user)
+        log_in(user)
+      end
 
-      click_link('Destroy')
-
-      expect(page).to have_content('Category List')
-      expect(page).to have_selector('div.alert.alert-success')
+      it 'categories/indexページが表示されること' do
+        visit category_path(@category)
+        click_link('Destroy')
+        expect(page).to have_selector('h3', text: 'Category List')
+      end
+      it 'フラッシュメッセージが表示されること' do
+        visit category_path(@category)
+        click_link('Destroy')
+        expect(page).to have_selector('div', text: 'Successful deleted category!')
+      end
+    end
+    context '未ログインの場合' do
+      it 'ログインページにリダイレクトされること' do
+        visit category_path(@category)
+        click_link('Destroy')
+        expect(page).to have_selector('h3', text: 'Log in')
+      end
+      it 'フラッシュメッセージが表示されること' do
+        visit category_path(@category)
+        click_link('Destroy')
+        expect(page).to have_selector('div', text: 'Please log in.')
+      end
     end
   end
 end
