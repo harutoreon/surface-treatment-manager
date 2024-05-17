b33bcc23b54522857bf70c747415007967732482
harutoreon <norabder567@gmail.com>
2024-05-18 07:25:00 +0900

    Modify system spec of catefories

--- spec/system/categories_management_flow_spec.rb
+++ spec/system/categories_management_flow_spec.rb
@@ -1,79 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe "CategoriesManagementFlow", type: :system do
-  before do
-    @category = FactoryBot.create(:category)
-  end
-
-  describe 'categories#create' do
-    context '有効な値を入力した場合' do
-      it 'categories/showページが表示されること' do
+  describe '#create' do
+    context '有効な値の場合' do
+      it '登録に成功すること' do
         visit new_category_path
         fill_in('Item',    with: '溶射')
-        fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
+        fill_in('Summary', with: 'セラミックスなどを基材表面に噴きつけて機能皮膜を形成する処理。')
         click_button('Create Category')
-        expect(page).to have_selector('h3', text: 'Category Information')
-      end
-      it 'フラッシュメッセージが表示されること' do
-        visit new_category_path
-        fill_in('Item',    with: '溶射')
-        fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
-        click_button('Create Category')
+        expect(page).to have_selector('h3',  text: 'Category Information')
         expect(page).to have_selector('div', text: 'Successful registration of new category!')
       end
     end
-    context '無効な値を入力した場合' do
-      it 'categories/newページが表示されること' do
+    context '無効な値の場合' do
+      it '登録に失敗すること' do
         visit new_category_path
         fill_in('Item',    with: '')
-        fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
+        fill_in('Summary', with: 'セラミックスなどを基材表面に噴きつけて機能皮膜を形成する処理。')
         click_button('Create Category')
-        expect(page).to have_selector('h3', text: 'New Registration for Category')
-      end
-      it 'バリデーションエラーが表示されること' do
-        visit new_category_path
-        fill_in('Item',    with: '')
-        fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
-        click_button('Create Category')
+        expect(page).to have_selector('h3',  text: 'New Registration for Category')
         expect(page).to have_selector('div', text: 'Item can\'t be blank')
       end
     end
   end
 
-  describe 'categories#update' do
+  describe '#update' do
+    before do
+      @category = FactoryBot.create(:category)
+    end
+
     context 'ログイン済みで' do
       before do
         user = FactoryBot.create(:user)
         log_in(user)
       end
 
-      context '有効な値を入力した場合' do
-        it 'categories/showページが表示されること' do
+      context '有効な値の場合' do
+        it '更新に成功すること' do
           visit edit_category_path(@category)
-          fill_in('Item', with: '溶射')
-          fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
+          fill_in('Item',    with: '塗装')
+          fill_in('Summary', with: '塗料によって固体表面に塗膜を形成させる加工方法のこと。')
           click_button('Update Category')
-          expect(page).to have_selector('h3', text: 'Category Information')
-        end
-        it 'フラッシュメッセージが表示されること' do
-          visit edit_category_path(@category)
-          fill_in('Item', with: '溶射')
-          fill_in('Summary', with: 'セラミックスやサーメットなどを熱源を用い溶融噴射し、基材表面に噴きつけて機能皮膜を形成する処理。')
-          click_button('Update Category')
+          expect(page).to have_selector('h3',  text: 'Category Information')
           expect(page).to have_selector('div', text: 'Successful updated category information!')
         end
       end
-      context '無効な値を入力した場合' do
-        it 'categories/editページが表示されること' do
+      context '無効な値の場合' do
+        it '更新に失敗すること' do
           visit edit_category_path(@category)
           fill_in('Item', with: '')
           click_button('Update Category')
-          expect(page).to have_selector('h3', text: 'Edit for Category')
-        end
-        it 'バリデーションエラーが出ること' do
-          visit edit_category_path(@category)
-          fill_in('Item', with: '')
-          click_button('Update Category')
+          expect(page).to have_selector('h3',  text: 'Edit for Category')
           expect(page).to have_selector('div', text: 'Item can\'t be blank')
         end
       end
@@ -81,30 +58,27 @@ RSpec.describe "CategoriesManagementFlow", type: :system do
     context '未ログインの場合' do
       it 'ログインページにリダイレクトされること' do
         visit edit_category_path(@category)
-        expect(page).to have_selector('h3', text: 'Log in')
-      end
-      it 'フラッシュメッセージが表示されること' do
-        visit edit_category_path(@category)
+        expect(page).to have_selector('h3',  text: 'Log in')
         expect(page).to have_selector('div', text: 'Please log in.')
       end
     end
   end
 
-  describe 'categories#destory' do
+  describe '#destory' do
+    before do
+      @category = FactoryBot.create(:category)
+    end
+
     context 'ログイン済みの場合' do
       before do
         user = FactoryBot.create(:user)
         log_in(user)
       end
 
-      it 'categories/indexページが表示されること' do
+      it '削除に成功すること' do
         visit category_path(@category)
         click_link('Destroy')
-        expect(page).to have_selector('h3', text: 'Category List')
-      end
-      it 'フラッシュメッセージが表示されること' do
-        visit category_path(@category)
-        click_link('Destroy')
+        expect(page).to have_selector('h3',  text: 'Category List')
         expect(page).to have_selector('div', text: 'Successful deleted category!')
       end
     end
@@ -112,11 +86,7 @@ RSpec.describe "CategoriesManagementFlow", type: :system do
       it 'ログインページにリダイレクトされること' do
         visit category_path(@category)
         click_link('Destroy')
-        expect(page).to have_selector('h3', text: 'Log in')
-      end
-      it 'フラッシュメッセージが表示されること' do
-        visit category_path(@category)
-        click_link('Destroy')
+        expect(page).to have_selector('h3',  text: 'Log in')
         expect(page).to have_selector('div', text: 'Please log in.')
       end
     end
