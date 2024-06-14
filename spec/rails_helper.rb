983b87a0ccc186060197f679d0494788315902ea
harutoreon <norabder567@gmail.com>
2024-06-15 05:38:14 +0900

    Grouping of measurement results

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 require 'simplecov'
 
-SimpleCov.start 'rails'
+SimpleCov.start do
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers',     'app/helpers'
+  add_group 'Models',      'app/models'
+  add_group 'Uploaders',   'app/uploaders'
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 
