e5b7e44d932c764702c9bf8579c21720d30a0516
harutoreon <norabder567@gmail.com>
2024-06-23 04:46:31 +0900

    Delete self.digest method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
-  validates :name,                  presence: true, length: { maximum: 50 }
-  validates :password,              presence: true, length: { minimum: 6 }, allow_nil: true
-
+  validates :name,     presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
-
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
 end
