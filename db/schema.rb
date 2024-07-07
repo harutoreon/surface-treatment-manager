b10d3f0f0a9c70411af2e88bd2439d497f268285
harutoreon <norabder567@gmail.com>
2024-07-07 22:33:55 +0900

    Add department attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_06_122133) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_07_131458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_06_122133) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "department"
     t.index ["name"], name: "index_users_on_name"
   end
 
