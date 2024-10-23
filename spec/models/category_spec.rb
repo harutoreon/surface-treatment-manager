30cb2358b285133187831cd59bc35a635dd04739
harutoreon <norabder567@gmail.com>
2024-10-24 01:24:17 +0900

    Change method from create to build

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,30 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'validation' do
-    let(:category) { FactoryBot.create(:category) }
+    before do
+      @category = FactoryBot.build(:category)
+    end
 
     it 'カテゴリが有効であること' do
-      expect(category).to be_valid
+      expect(@category).to be_valid
     end
 
     it 'itemが存在すること' do
-      category.item = ''
-      expect(category).to_not be_valid
+      @category.item = ''
+      expect(@category).to_not be_valid
     end
 
     it 'summaryが存在すること' do
-      category.summary = ''
-      expect(category).to_not be_valid
+      @category.summary = ''
+      expect(@category).to_not be_valid
     end
 
     it 'itemの重複がないこと' do
-      duplicate_category = category.dup
+      @category.save
+      duplicate_category = @category.dup
       expect(duplicate_category).to_not be_valid
     end
 
     it 'summaryの長さが60文字以内であること' do
-      category.summary = 's' * 61
-      expect(category).to_not be_valid
+      @category.summary = 's' * 61
+      expect(@category).to_not be_valid
     end
   end
 end
