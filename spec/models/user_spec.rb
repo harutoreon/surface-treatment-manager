a09e1151655fd55aa2bac6edb104a95134de5720
harutoreon <norabder567@gmail.com>
2024-10-25 04:56:54 +0900

    Refactor pretreatment cutout

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,39 +2,45 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validation' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
     it 'userが有効であること' do
-      user = FactoryBot.build(:user)
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it 'nameが存在すること' do
-      user = FactoryBot.build(:user, name: '')
-      expect(user).to_not be_valid
+      @user.name = ''
+      expect(@user).to_not be_valid
     end
 
     it 'nameは50文字以下であること' do
-      user = FactoryBot.build(:user, name: 's' * 51)
-      expect(user).to_not be_valid
+      @user.name = 's' * 51
+      expect(@user).to_not be_valid
     end
 
     it 'departmentが存在すること' do
-      user = FactoryBot.build(:user, department: '')
-      expect(user).to_not be_valid
+      @user.department = ''
+      expect(@user).to_not be_valid
     end
 
     it 'パスワードが空白でないこと' do
-      user = FactoryBot.build(:user, password: '', password_confirmation: '')
-      expect(user).to_not be_valid
+      @user.password = ''
+      @user.password_confirmation = ''
+      expect(@user).to_not be_valid
     end
 
     it 'パスワードが6文字以上であること' do
-      user = FactoryBot.build(:user, password: 's' * 5, password_confirmation: 's' * 5)
-      expect(user).to_not be_valid
+      @user.password = 's' * 5
+      @user.password_confirmation = 's' * 5
+      expect(@user).to_not be_valid
     end
 
     it 'パスワードの組み合わせが正常であること' do
-      user = FactoryBot.build(:user, password: 'foobar', password_confirmation: 'foobaz')
-      expect(user).to_not be_valid
+      @user.password = 'foobar'
+      @user.password_confirmation = 'foobaz'
+      expect(@user).to_not be_valid
     end
   end
 end
