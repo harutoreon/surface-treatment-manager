00906c3b77002d5680f66d4d40f94e37f1a048a9
harutoreon <norabder567@gmail.com>
2024-10-27 18:14:52 +0900

    Add cop and modify code style

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Users", type: :request do
       get users_path
       expect(response).to have_http_status(:success)
     end
+
     it 'タイトルが表示されること' do
       get users_path
       expect(response.body).to include('<title>ユーザーリスト</title>')
@@ -22,6 +23,7 @@ RSpec.describe "Users", type: :request do
       get user_path(@user)
       expect(response).to have_http_status(:success)
     end
+
     it 'タイトルが表示されること' do
       get user_path(@user)
       expect(response.body).to include('<title>ユーザー情報</title>')
@@ -33,6 +35,7 @@ RSpec.describe "Users", type: :request do
       get new_user_path
       expect(response).to have_http_status(:success)
     end
+
     it 'タイトルが表示されること' do
       get new_user_path
       expect(response.body).to include('<title>ユーザー情報の登録</title>')
@@ -50,18 +53,21 @@ RSpec.describe "Users", type: :request do
       it '登録が成功すること' do
         expect{ post users_path, params: user_params }.to change{ User.count }.from(1).to(2)
       end
+
       it 'users/showページにリダイレクトすること' do
         post users_path, params: user_params
         new_user = User.last
         expect(response).to redirect_to new_user
       end
     end
+
     context '無効なユーザー情報のとき' do
       let(:user_params) { { user: { name: "", department: "品質管理部", password: "password", password_confirmation: "password" } } }
 
       it '登録が失敗すること' do
         expect{ post users_path, params: user_params }.to_not change{ User.count }.from(1)
       end
+
       it 'users/newページが表示されること' do
         post users_path, params: user_params
         expect(response.body).to include('ユーザー情報の登録')
@@ -80,11 +86,13 @@ RSpec.describe "Users", type: :request do
         get edit_user_path(@user)
         expect(response).to have_http_status(:success)
       end
+
       it 'タイトルが表示されること' do
         get edit_user_path(@user)
         expect(response.body).to include('<title>ユーザー情報の編集</title>')
       end
     end
+
     context '未ログインの場合' do
       before do
         @user = FactoryBot.create(:user)
@@ -94,6 +102,7 @@ RSpec.describe "Users", type: :request do
         get edit_user_path(@user)
         assert_redirected_to login_url
       end
+
       it 'フラッシュメッセージが表示されること' do
         get edit_user_path(@user)
         expect(flash[:danger]).to eq('ログインしてください')
@@ -117,32 +126,38 @@ RSpec.describe "Users", type: :request do
           @user.reload
           expect(@user.name).to eq('sample user')
         end
+
         it 'users/showページにリダイレクトされること' do
           patch user_path(@user), params: { user: { name: 'sample user', password: 'password', password_confirmation: 'password' } }
           expect(response).to redirect_to @user
         end
+
         it 'フラッシュメッセージが表示されていること' do
           patch user_path(@user), params: { user: { name: 'sample user', password: 'password', password_confirmation: 'password' } }
           expect(flash).to be_any
         end
       end
+
       context '無効な値の場合' do
         it '更新できないこと' do
           patch user_path(@user), params: { user: { name: '' } }
           @user.reload
           expect(@user.name).to_not eq('')
         end
+
         it 'users/editページが表示されること' do
           patch user_path(@user), params: { user: { name: '' } }
           expect(response.body).to include('ユーザー情報の編集')
         end
       end
     end
+
     context '未ログインの場合' do
       it "ログインページにリダイレクトされること" do
         patch user_path(@user), params: { user: { name: 'sample user' } }
         assert_redirected_to login_url
       end
+
       it 'フラッシュメッセージが表示されること' do
         patch user_path(@user), params: { user: { name: 'sample user' } }
         expect(flash[:danger]).to eq('ログインしてください')
@@ -164,12 +179,14 @@ RSpec.describe "Users", type: :request do
       it '削除に成功すること' do
         expect { delete user_path(@sample_user) }.to change{ User.count }.from(2).to(1)
       end
+
       it 'users/indexページにリダイレクトされること' do
         delete user_path(@sample_user)
         expect(response).to redirect_to users_url
         expect(flash[:success]).to eq('ユーザーの削除に成功しました!')
       end
     end
+
     context '一般ユーザーでログインした場合' do
       before do
         @general_user = FactoryBot.create(:general_user)
@@ -179,11 +196,13 @@ RSpec.describe "Users", type: :request do
       it '削除に失敗すること' do
         expect { delete user_path(@sample_user) }.to_not change{ User.count }.from(2)
       end
+
       it 'ログインページにリダイレクトされること' do
         delete user_path(@sample_user)
         expect(response).to redirect_to login_url
       end
     end
+
     context '未ログインの場合' do
       it '削除に失敗すること' do
         expect { delete user_path(@sample_user) }.to_not change{ User.count }.from(1)
