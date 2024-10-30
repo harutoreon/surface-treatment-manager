ff9c138080ff1010e6f27c179bb2589fac7c3254
harutoreon <norabder567@gmail.com>
2024-10-31 03:01:50 +0900

    Add blank line

--- spec/requests/makers_spec.rb
+++ spec/requests/makers_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Makers", type: :request do
       get makers_path
       expect(response).to have_http_status(:success)
     end
+
     it 'タイトルが表示されること' do
       get makers_path
       expect(response.body).to include('<title>メーカーリスト</title>')
@@ -23,6 +24,7 @@ RSpec.describe "Makers", type: :request do
       get maker_path(@maker)
       expect(response).to have_http_status(:success)
     end
+
     it 'タイトルが表示されること' do
       get maker_path(@maker)
       expect(response.body).to include('<title>メーカー情報</title>')
@@ -34,6 +36,7 @@ RSpec.describe "Makers", type: :request do
       get new_maker_path
       expect(response).to have_http_status(:success)
     end
+
     it 'タイトルが表示されること' do
       get new_maker_path
       expect(response.body).to include('<title>メーカー情報の登録</title>')
@@ -54,6 +57,7 @@ RSpec.describe "Makers", type: :request do
       it '登録が成功すること' do
         expect { post makers_path, params: valid_params }.to change{ Maker.count }.from(0).to(1)
       end
+
       it 'makers/showにリダイレクトすること' do
         post makers_path, params: valid_params
         maker = Maker.last
@@ -74,6 +78,7 @@ RSpec.describe "Makers", type: :request do
       it '登録が失敗すること' do
         expect { post makers_path, params: invalid_params }.to_not change{ Maker.count }.from(0)
       end
+
       it 'makers/newが再描画されること' do
         post makers_path, params: invalid_params
         expect(response.body).to include("<title>メーカー情報の登録</title>")
@@ -96,16 +101,19 @@ RSpec.describe "Makers", type: :request do
         get edit_maker_path(@maker)
         expect(response).to have_http_status(:success)
       end
+
       it 'タイトルが表示されること' do
         get edit_maker_path(@maker)
         expect(response.body).to include('<title>メーカー情報の編集</title>')
       end
     end
+
     context '未ログインの場合' do
       it "ログインページにリダイレクトされること" do
         get edit_maker_path(@maker)
         assert_redirected_to login_url
       end
+
       it 'フラッシュメッセージが表示されること' do
         get edit_maker_path(@maker)
         expect(flash[:danger]).to eq('ログインしてください')
@@ -130,34 +138,40 @@ RSpec.describe "Makers", type: :request do
           @maker.reload
           expect(@maker.name).to eq('佐藤情報合名会社')
         end
+
         it 'makers/showにリダイレクトすること' do
           patch maker_path(@maker), params: { maker: { name: "佐藤情報合名会社" } }
           @maker.reload
           expect(response).to redirect_to(@maker)
         end
+
         it 'フラッシュメッセージが表示されること' do
           patch maker_path(@maker), params: { maker: { name: "佐藤情報合名会社" } }
           @maker.reload
           expect(flash[:success]).to eq('メーカーの更新に成功しました!')
         end
       end
+
       context '無効なパラメータの場合' do
         it '更新できないこと' do
           patch maker_path(@maker), params: { maker: { name: '' } }
           @maker.reload
           expect(@maker.name).to_not eq('')
         end
+
         it 'users/editページが表示されること' do
           patch maker_path(@maker), params: { maker: { name: '' } }
           expect(response.body).to include('メーカー情報の編集')
         end
       end
     end
+
     context '未ログインの場合' do
       it "ログインページにリダイレクトされること" do
         patch maker_path(@maker), params: { maker: { name: 'sample user' } }
         assert_redirected_to login_url
       end
+
       it 'フラッシュメッセージが表示されること' do
         patch maker_path(@maker), params: { user: { name: 'sample user' } }
         expect(flash[:danger]).to eq('ログインしてください')
@@ -179,12 +193,14 @@ RSpec.describe "Makers", type: :request do
       it '削除に成功すること' do
         expect { delete maker_path(@maker) }.to change{ Maker.count }.from(1).to(0)
       end
+
       it 'makers/indexページににリダイレクトすること' do
         delete maker_path(@maker)
         expect(response).to redirect_to(makers_url)
         expect(flash[:success]).to eq('メーカーの削除に成功しました!')
       end
     end
+
     context '一般ユーザーでログインした場合' do
       before do
         general_user = FactoryBot.create(:general_user)
@@ -196,6 +212,7 @@ RSpec.describe "Makers", type: :request do
         expect(response).to redirect_to(login_url)
       end
     end
+
     context '未ログインの場合' do
       it "ログインページにリダイレクトされること" do
         delete maker_path(@maker)
