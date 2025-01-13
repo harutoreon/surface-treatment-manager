5c5664666861a0f594773f377b0fab730a8c5a74
harutoreon <norabder567@gmail.com>
2025-01-13 19:44:09 +0900

    Settings CORS

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -25,5 +25,14 @@ module SurfaceTreatmentManager
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+      origins 'http://localhost:5173'
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
