c21d18e72de5bffd43af7023e1c91fac155f3bfa
harutoreon <norabder567@gmail.com>
2025-01-18 23:13:10 +0900

    Check communication environment between Frontend and Backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const data = ref([])
+
+const loadData = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/samples/2')
+    data.value = response.data
+  } catch (error) {
+    console.error('Failed to load data:', error)
+  }
+}
+
+onMounted(() => {
+  loadData()
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>表面処理情報</h2>
+  <p>表面処理名 : {{ data.name }}</p>
+  <p>カテゴリー : {{ data.category }}</p>
+  <p>カラー : {{ data.color }}</p>
+  <p>メーカー名 : {{ data.color }}</p>
+  <p>作成日 : {{ data.color }}</p>
+  <p>更新日 : {{ data.color }}</p>
+  <p>画像 : {{ data.picture }}</p>
+  <p>硬度 : {{ data.hardness }}</p>
+  <p>膜厚 : {{ data.film_thickness }}</p>
+  <p>特徴 : {{ data.feature }}</p>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
