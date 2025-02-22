ffb718774d74d09476dbf5b04baa76174582227f
reonharuto <norabder567@gmail.com>
2025-02-23 08:26:20 +0900

    Unique alt attribute value

--- frontend/src/components/HomeView.vue
+++ frontend/src/components/HomeView.vue
@@ -17,7 +17,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
     <div class="row mb-4">
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="experimentIcon">
+          <img alt="experiment icon" class="card-img-top w-25" width="50" height="50" v-bind:src="experimentIcon">
           <div class="card-body">
             <h5 class="card-title">処理名で検索</h5>
             <p class="card-text">処理名を入力して表面処理を検索します。</p>
@@ -27,7 +27,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
       </div>
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="categoryIcon">
+          <img alt="category icon" class="card-img-top w-25" width="50" height="50" v-bind:src="categoryIcon">
           <div class="card-body">
             <h5 class="card-title">カテゴリーで検索</h5>
             <p class="card-text">カテゴリーを選択して表面処理を検索します。</p>
@@ -37,7 +37,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
       </div>
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="factoryIcon">
+          <img alt="factory icon" class="card-img-top w-25" width="50" height="50" v-bind:src="factoryIcon">
           <div class="card-body">
             <h5 class="card-title">メーカー名で検索</h5>
             <p class="card-text">メーカー名を入力して表面処理を検索します。</p>
@@ -47,7 +47,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
       </div>
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="listIcon">
+          <img alt="list icon" class="card-img-top w-25" width="50" height="50" v-bind:src="listIcon">
           <div class="card-body">
             <h5 class="card-title">処理一覧から検索</h5>
             <p class="card-text">表面処理一覧から目的の処理を検索します。</p>
@@ -60,7 +60,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
     <div class="row mb-4">
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="libraryAddIcon">
+          <img alt="library add icon" class="card-img-top w-25" width="50" height="50" v-bind:src="libraryAddIcon">
           <div class="card-body">
             <h5 class="card-title">表面処理の管理</h5>
             <p class="card-text">表面処理に関する情報を一括管理します。</p>
@@ -70,7 +70,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
       </div>
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="categoryAddIcon">
+          <img alt="category add icon" class="card-img-top w-25" width="50" height="50" v-bind:src="categoryAddIcon">
           <div class="card-body">
             <h5 class="card-title">カテゴリーの管理</h5>
             <p class="card-text">カテゴリーに関する情報を一括管理します。</p>
@@ -80,7 +80,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
       </div>
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="makerAddIcon">
+          <img alt="maker add icon" class="card-img-top w-25" width="50" height="50" v-bind:src="makerAddIcon">
           <div class="card-body">
             <h5 class="card-title">メーカーの管理</h5>
             <p class="card-text">メーカーに関する情報を一括管理します。</p>
@@ -90,7 +90,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
       </div>
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="userAddIcon">
+          <img alt="user add icon" class="card-img-top w-25" width="50" height="50" v-bind:src="userAddIcon">
           <div class="card-body">
             <h5 class="card-title">ユーザーの管理</h5>
             <p class="card-text">ユーザーに関する情報を一括管理します。</p>
@@ -103,7 +103,7 @@ import settingsIcon from '@/assets/icons/settings.svg'
     <div class="row mb-4">
       <div class="col ps-0 pe-0">
         <div class="card pt-2" style="width: 19rem;">
-          <img alt="sample image" class="card-img-top w-25" width="50" height="50" v-bind:src="settingsIcon">
+          <img alt="settings icon" class="card-img-top w-25" width="50" height="50" v-bind:src="settingsIcon">
           <div class="card-body">
             <h5 class="card-title">アプリケーションの管理</h5>
             <p class="card-text">アプリケーションの設定やログアウトを行います。</p>
