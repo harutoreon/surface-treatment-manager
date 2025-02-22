7237b184e4ca0bdd8fb43a6ece16168f152184bc
reonharuto <norabder567@gmail.com>
2025-02-22 15:55:56 +0900

    Add HomeView.vue

---
+++ frontend/src/components/HomeView.vue
@@ -0,0 +1,107 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container w-75">
+    <h3 class="text-center mt-5 mb-5">メインメニュー</h3>
+
+    <div class="row mb-4">
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/experiment-6d9d1ff13d383f8358dca3f347d188d287466cfb46ee232ae909610244348804.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">処理名で検索</h5>
+            <p class="card-text">処理名を入力して表面処理を検索します。</p>
+            <!-- <a class="card-link" href="/category/name">検索ページへ</a> -->
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/category-fdcf0ac019e3e3a85c2d8fc90ffa2aa34ab74beca04bac2549816a77a613cb44.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">カテゴリーで検索</h5>
+            <p class="card-text">カテゴリーを選択して表面処理を検索します。</p>
+            <!-- <a class="card-link" href="/category/category">検索ページへ</a> -->
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/factory-d733e0d35cf9438dc223767d2f0a0c3737a67359a7500e0d17574c6132e5dd82.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">メーカー名で検索</h5>
+            <p class="card-text">メーカー名を入力して表面処理を検索します。</p>
+            <!-- <a class="card-link" href="/category/maker">検索ページへ</a> -->
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/list-b5c64ec38b361b8d350f25637a4dac7c0b1768c1c50ff9ea8bea4ca59bd493a3.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">処理一覧から検索</h5>
+            <p class="card-text">表面処理一覧から目的の処理を検索します。</p>
+            <!-- <a class="card-link" href="/samples">検索ページへ</a> -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/library_add-48302094130ea78dacd4a2901d593dc94c465eb8e5463cab03a17d60cd3cef6f.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">表面処理の管理</h5>
+            <p class="card-text">表面処理に関する情報を一括管理します。</p>
+            <!-- <a class="card-link" href="/samples">管理ページへ</a> -->
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/category_add-da840e2a3a949ca0b8ffdc4d725331e26acdfbb086d247703976b98455c27937.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">カテゴリーの管理</h5>
+            <p class="card-text">カテゴリーに関する情報を一括管理します。</p>
+            <!-- <a class="card-link" href="/categories">管理ページへ</a> -->
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/maker_add-e53c4be04bc6e3a0e7549e338ced9b388bb79332ac9ba46c4f13ca17fa5cc6a8.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">メーカーの管理</h5>
+            <p class="card-text">メーカーに関する情報を一括管理します。</p>
+            <!-- <a class="card-link" href="/makers">管理ページへ</a> -->
+          </div>
+        </div>
+      </div>
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/user_add-8f7326fe6391e9fbdf8c5683f90deb19bb1e93002022ddd855eecd39789fbb86.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">ユーザーの管理</h5>
+            <p class="card-text">ユーザーに関する情報を一括管理します。</p>
+            <!-- <a class="card-link" href="/users">管理ページへ</a> -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col ps-0 pe-0">
+        <div class="card pt-2" style="width: 19rem;">
+          <!-- <img alt="sample image" class="card-img-top w-25" width="50" height="50" src="/assets/settings-4b48f190ccda6a0a78bb1d20e7f7831ea5a181d826e0f9f74f969b3a7616b321.svg"> -->
+          <div class="card-body">
+            <h5 class="card-title">アプリケーションの管理</h5>
+            <p class="card-text">アプリケーションの設定やログアウトを行います。</p>
+            <!-- <a class="card-link" href="/setting">管理ページへ</a> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,15 +1,16 @@
-import { createRouter, createWebHistory, createMemoryHistory } from "vue-router"
+import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
 
-import LoginForm from "@/components/LoginForm.vue"
-import SamplesView from "@/components/SamplesView.vue"
-import SettingsView from "@/components/SettingsView.vue"
+import LoginForm from '@/components/LoginForm.vue'
+import HomeView from '@/components/HomeView.vue'
+import SettingsView from '@/components/SettingsView.vue'
+
 
 const history = import.meta.env.MODE === 'test' ? createMemoryHistory() : createWebHistory()
 
 const routes = [   
   { path: '/', component: LoginForm },
-  { path: '/sample', component: SamplesView },
-  { path: '/settings', component: SettingsView }
+  { path: '/home', component: HomeView },
+  { path: '/settings', component: SettingsView },
 ]
 
 const router = createRouter({
