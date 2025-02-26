fa8aecdaa44e0244804d0d1fefc83867b4ce4792
reonharuto <norabder567@gmail.com>
2025-02-27 03:50:39 +0900

    Add UsersShowView.test.js

---
+++ frontend/test/component/UsersShowView.test.js
@@ -0,0 +1,56 @@
+import { mount } from '@vue/test-utils'
+import UsersShowView from '@/components/UsersShowView.vue'
+import axios from 'axios'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
+
+vi.mock('vue-router', () => ({
+  useRoute: () => ({
+    params: { id: '1' }
+  })
+}))
+
+vi.mock('axios')
+
+describe('UsersShowView', () => {
+  let wrapper
+
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: {
+        name: 'test_user',
+        department: 'sample_department'
+      }
+    })
+    wrapper = mount(UsersShowView, {
+      global: {
+        stubs: {
+          RouterLink: {
+            props: ['to'],
+            template:'<a v-bind:href="to"><slot /></a>'
+          }
+        }
+      }
+    })  
+  })
+
+  describe('コンポーネントのレンダリング', () => {
+    it('見出し「ユーザー情報」が表示されること', () => {
+      expect(wrapper.find('h3').text()).toBe('ユーザー情報')
+    })
+
+    it('ユーザー名と部署名が表示されること', async () => {  
+      await wrapper.vm.$nextTick()
+      const elements = wrapper.findAll('.list-group-item')
+  
+      expect(elements[0].find('div:nth-child(2)').text()).toBe('test_user')
+      expect(elements[1].find('div:nth-child(2)').text()).toBe('sample_department')
+    })
+
+    it('RouterLinkのto属性が/usersであること', () => {
+      const links = wrapper.findAll('a')
+
+      expect(links[0].attributes('href')).toBe('#')
+      expect(links[1].attributes('href')).toBe('/users')
+    })
+  })
+})
