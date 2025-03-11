f119810c16507e6aa4edb7aec0465a392fd5cd76
reonharuto <norabder567@gmail.com>
2025-03-11 20:12:50 +0900

    Link config

--- frontend/src/components/categories/CategoriesIndexView.vue
+++ frontend/src/components/categories/CategoriesIndexView.vue
@@ -1,3 +1,7 @@
+<script setup>
+
+</script>
+
 <template>
   <div class="container w-50">
     <h3 class="text-center mt-5 mb-5">カテゴリーリスト</h3>
@@ -10,41 +14,45 @@
         </div>
       </div>
 
-      <a class="list-group-item list-group-item-action" href="/categories/1">
+      <RouterLink to="#" class="list-group-item list-group-item-action">
         <div class="d-flex w-100 justify-content-between">
           <h6>めっき</h6>
           <h6>金属または非金属の材料の表面に金属の薄膜を被覆する処理のこと。</h6>
-        </div>
-      </a>
-      <a class="list-group-item list-group-item-action" href="/categories/2">
+        </div>        
+      </RouterLink>
+
+      <RouterLink to="#" class="list-group-item list-group-item-action">
         <div class="d-flex w-100 justify-content-between">
           <h6>陽極酸化</h6>
           <h6>人工的にアルミニウム表面に分厚い酸化アルミニウム被膜を作る処理のこと。</h6>
         </div>
-      </a>
-      <a class="list-group-item list-group-item-action" href="/categories/3">
+      </RouterLink>
+
+      <RouterLink to="#" class="list-group-item list-group-item-action">
         <div class="d-flex w-100 justify-content-between">
           <h6>化成</h6>
           <h6>金属の表面に処理剤を作用させて化学反応を起こさせる処理のこと。</h6>
         </div>
-      </a>
-      <a class="list-group-item list-group-item-action" href="/categories/4">
+      </RouterLink>
+
+      <RouterLink to="#" class="list-group-item list-group-item-action">
         <div class="d-flex w-100 justify-content-between">
           <h6>コーティング</h6>
           <h6>溶射金属やセラミックスの粉末を、溶解状態にして製品表面に吹き付ける処理のこと。</h6>
         </div>
-      </a>
-      <a class="list-group-item list-group-item-action" href="/categories/5">
+      </RouterLink>
+
+      <RouterLink to="#" class="list-group-item list-group-item-action">
         <div class="d-flex w-100 justify-content-between">
           <h6>表面硬化</h6>
           <h6>主に金属材料に対して行われる、加熱・冷却・雰囲気により材料の性質を変化させる処理のこと。</h6>
         </div>
-      </a>
+      </RouterLink>
     </div>
 
     <div class="d-flex justify-content-evenly">
-      <a href="/categories/new">カテゴリー情報の登録</a>
-      <a href="/home">メインメニューへ</a>
+      <RouterLink to="#">カテゴリー情報の登録</RouterLink>
+      <RouterLink to="/home">メインメニューへ</RouterLink>
     </div>
   </div>
 </template>
