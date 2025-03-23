67d99d7e9d8ab2f02d30513755b697fc414b97ee
reonharuto <norabder567@gmail.com>
2025-03-24 04:56:36 +0900

    Add api communication

--- frontend/src/components/categories/CategoriesNewView.vue
+++ frontend/src/components/categories/CategoriesNewView.vue
@@ -1,13 +1,41 @@
+<script setup>
+  import { ref } from 'vue'
+  import axios from 'axios'
+  import router from '@/router'
+
+  const item = ref('')
+  const summary = ref('')
+  const category = ref('')
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+  const categoryRegistration = async () => {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/categories`, {
+        category: {
+          item: item.value,
+          summary: summary.value
+        }
+      })
+      category.value = response.data
+      console.log(category.value)
+      router.push(`/categories/${category.value.id}`)
+    } catch (error) {
+      console.error('Category registration failed.')
+    }
+  }
+
+</script>
+
 <template>
   <div class="container w-25">
     <h3 class="text-center mt-5 mb-5">カテゴリー情報の登録</h3>
 
-    <form>
+    <form v-on:submit.prevent="categoryRegistration">
       <label class="form-label" for="category_item">カテゴリー名</label>
-      <input class="form-control mb-4" type="text" name="category[item]" id="category_item" />
+      <input v-model="item" class="form-control mb-4" type="text" id="category_item" required/>
 
       <label class="form-label" for="category_summary">概要</label>
-      <input class="form-control mb-4" type="text" name="category[summary]" id="category_summary" />
+      <input v-model="summary" class="form-control mb-4" type="text" id="category_summary" required/>
 
       <input type="submit" name="commit" value="登録" class="form-control btn btn-primary mb-5" data-disable-with="登録" />
     </form>
