4c2dd3fb389ee1004435dbcea19d259e8ed47483
reonharuto <norabder567@gmail.com>
2025-04-01 02:36:34 +0900

    Add routing test

---
+++ frontend/test/e2e/routing.test.js
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
+import router from '@/router'
+
+describe('Makers routing', () => {
+  it('「メーカーリスト」ページに遷移できること', async () => {
+    router.push('/makers')
+
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    expect(wrapper.html()).toContain('メーカーリスト')
+  })
+})
