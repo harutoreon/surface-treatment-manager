b19936a954b8db4a5428462d2151559604e99afb
reonharuto <norabder567@gmail.com>
2025-04-02 04:15:27 +0900

    Add api communication

--- frontend/src/components/makers/MakersIndexView.vue
+++ frontend/src/components/makers/MakersIndexView.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const route = useRoute()
+const makers = ref([])
+const currentPage = ref(Number(route.query.page) || 1)
+const totalPages = ref(1)
+
+const fetchMakerList = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/makers?page=${currentPage.value}`)
+    const data = response.data
+    makers.value = data.makers
+    currentPage.value = data.current_page
+    totalPages.value = data.total_pages
+  } catch (error) {
+    console.error('Get maker list failed')
+  }
+}
+
+const getPageLink = (page) => ({
+  path: route.path,
+  query: { page }
+})
+
+watch(() => route.query.page, (newPage) => {
+  currentPage.value = Number(newPage) || 1
+  fetchMakerList()
+}, { immediate: true })
+
+onMounted(() => {
+  fetchMakerList()
+})
+</script>
+
 <template>
   <div class="container w-50">
     <h3 class="text-center mt-5 mb-5">メーカーリスト</h3>
@@ -10,143 +48,31 @@
         </div>
       </div>
 
-      <a class="list-group-item list-group-item-action" href="#">
+      <RouterLink v-for="maker in makers" v-bind:key="maker.id" class="list-group-item list-group-item-action" to="#">
         <div class="d-flex justify-content-between">
-          <h6>長谷川水産合資会社</h6>
-          <small>TEL: 080-5808-0222</small>
+          <h6>{{ maker.name }}</h6>
+          <small>{{ maker.phone_number }}</small>
         </div>
         <div class="d-flex justify-content-between">
-          <small>〒106-6145 東京都渋谷区神南1-2-0</small>
-          <small>FAX: 070-9532-2874</small>
+          <small>{{ maker.address }}</small>
+          <small>{{ maker.fax_number }}</small>
         </div>
-      </a>
-
-      <a class="list-group-item list-group-item-action" href="#">
-        <div class="d-flex justify-content-between">
-          <h6>大野印刷株式会社</h6>
-          <small>TEL: 090-1157-4630</small>
-        </div>
-        <div class="d-flex justify-content-between">
-          <small>〒590-0928 東京都渋谷区神南1-2-1</small>
-          <small>FAX: 070-8662-0619</small>
-        </div>
-      </a>
-
-      <a class="list-group-item list-group-item-action" href="#">
-        <div class="d-flex justify-content-between">
-          <h6>佐々木食品株式会社</h6>
-          <small>TEL: 080-6616-2999</small>
-        </div>
-        <div class="d-flex justify-content-between">
-          <small>〒039-4152 東京都渋谷区神南1-2-2</small>
-          <small>FAX: 070-6702-6634</small>
-        </div>
-      </a>
-      
-      <a class="list-group-item list-group-item-action" href="#">
-        <div class="d-flex justify-content-between">
-          <h6>合資会社山本印刷</h6>
-          <small>TEL: 090-3466-6784</small>
-        </div>
-        <div class="d-flex justify-content-between">
-          <small>〒479-0045 東京都渋谷区神南1-2-3</small>
-          <small>FAX: 080-6811-0656</small>
-        </div>
-      </a>
-      
-      <a class="list-group-item list-group-item-action" href="#">
-        <div class="d-flex justify-content-between">
-          <h6>合名会社山田印刷</h6>
-          <small>TEL: 080-9274-1520</small>
-        </div>
-        <div class="d-flex justify-content-between">
-          <small>〒761-4423 東京都渋谷区神南1-2-4</small>
-          <small>FAX: 090-5827-2604</small>
-        </div>
-      </a>
-      
-      <a class="list-group-item list-group-item-action" href="#">
-        <div class="d-flex justify-content-between">
-          <h6>金子農林合名会社</h6>
-          <small>TEL: 070-6909-6952</small>
-        </div>
-        <div class="d-flex justify-content-between">
-          <small>〒292-0801 東京都渋谷区神南1-2-5</small>
-          <small>FAX: 070-8503-0054</small>
-        </div>
-      </a>
-      
-      <a class="list-group-item list-group-item-action" href="#">
-        <div class="d-flex justify-content-between">
-          <h6>合資会社杉山ガス</h6>
-          <small>TEL: 080-2228-3988</small>
-        </div>
-        <div class="d-flex justify-content-between">
-          <small>〒612-8484 東京都渋谷区神南1-2-6</small>
-          <small>FAX: 070-1981-8467</small>
-        </div>
-      </a>      
+      </RouterLink>
     </div>
 
-    <ul class="pagination justify-content-center mb-5">
-      <li class="page-item disabled">
-        <a class="page-link">前のページ</a>
+    <ul v-if="totalPages > 0"  class="pagination justify-content-center mb-5">
+      <li class="page-item" :class="{ disabled: currentPage === 1 }">
+        <RouterLink class="page-link" v-if="currentPage > 1" v-bind:to="getPageLink(currentPage - 1)">前ページ</RouterLink>
+        <span v-else class="page-link">前ページ</span>
       </li>
-      
-      <li class="page-item active">
-        <a class="page-link" href="#">1</a>
+
+      <li v-for="page in totalPages" v-bind:key="page" class="page-item" v-bind:class="{ active: page === currentPage }">
+        <RouterLink class="page-link" v-bind:to="getPageLink(page)">{{ page }}</RouterLink>
       </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">2</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">3</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">4</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">5</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">6</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">7</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">8</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">9</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">10</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">11</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">12</a>
-      </li>
-      
-      <li class="page-item ">
-        <a class="page-link" href="#">13</a>
-      </li>
-      
-      <li class="page-item">
-        <a class="page-link" href="#">次のページ</a>
+
+      <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+        <RouterLink class="page-link" v-if="currentPage < totalPages" v-bind:to="getPageLink(currentPage + 1)">次ページ</RouterLink>
+        <span v-else class="page-link">次ページ</span>
       </li>
     </ul>
 
