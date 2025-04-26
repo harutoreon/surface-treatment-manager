1d0df141b6734422784218dba0a4e5ae81ee5b83
reonharuto <norabder567@gmail.com>
2025-04-27 05:17:09 +0900

    Enable url_for method in production environment

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  Rails.application.routes.default_url_options[:host] = 'https://surface-treatment-manager.onrender.com'
+  Rails.application.routes.default_url_options[:protocol] = 'https'
 end
