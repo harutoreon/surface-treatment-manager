ddff0ee64c8da82dd6984c77818e6c22bb31e1e5
reonharuto <norabder567@gmail.com>
2025-05-06 12:06:24 +0900

    Tweak code style and UI

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -38,23 +38,45 @@ const changeToGeneralUserData = () => {
 
 <template>
   <div class="container w-25">
-
     <h3 class="text-center mt-5 mb-5">ログイン</h3>
 
     <label>ユーザーの選択</label>
-
     <ul class="list-group mb-4">
       <li class="list-group-item">
         <div class="form-check">
-          <input v-model="selectedUserType" v-on:change="changeToGeneralUserData" class="form-check-input" type="radio" id="general_user" value="general">
-          <label class="form-check-label" for="general_user">一般ユーザー</label>
+          <input
+            v-model="selectedUserType"
+            v-on:change="changeToGeneralUserData"
+            class="form-check-input"
+            type="radio"
+            id="general_user"
+            value="general"
+          >
+          <label
+            class="form-check-label"
+            for="general_user"
+          >
+            一般ユーザー
+          </label>
         </div>
         <small class="ms-4">一部の機能は制限されます。</small>
       </li>
       <li class="list-group-item">
         <div class="form-check">
-          <input v-model="selectedUserType" v-on:change="changeToAdminUserData" class="form-check-input" type="radio"  id="admin_user" value="admin">
-          <label class="form-check-label" for="admin_user">管理者ユーザー</label>
+          <input
+            v-model="selectedUserType"
+            v-on:change="changeToAdminUserData"
+            class="form-check-input"
+            type="radio"
+            id="admin_user"
+            value="admin"
+          >
+          <label
+            class="form-check-label"
+            for="admin_user"
+          >
+            管理者ユーザー
+          </label>
         </div>
         <small class="ms-4">すべての機能が利用できます。</small>
       </li>
@@ -62,13 +84,35 @@ const changeToGeneralUserData = () => {
 
     <form v-on:submit.prevent="handleLogin">
       <label for="name">ユーザー名</label>
-      <input v-model="name" class="form-control" type="text" id="user_name" required><br>
-
+      <input
+        v-model="name"
+        class="form-control mb-4"
+        type="text"
+        id="user_name"
+        required
+      >
       <label for="password">パスワード</label>
-      <input v-model="password" class="form-control mb-4" type="password" id="password" required><br>
-
-      <button type="submit" class="form-control btn btn-primary">ログイン</button>
+      <input
+        v-model="password"
+        class="form-control mb-5"
+        type="password"
+        id="password"
+        required
+      >
+      <button
+        type="submit"
+        class="form-control btn btn-primary"
+      >
+        ログイン
+      </button>
     </form>
-    <p v-if="errorMessage" class="alert alert-danger mt-4" role="alert">{{ errorMessage }}</p>
+
+    <p
+      v-if="errorMessage"
+      class="alert alert-danger mt-4"
+      role="alert"
+    >
+      {{ errorMessage }}
+    </p>
   </div>
 </template>
