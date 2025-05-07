4218b7ccb957f0ecce97c07de584739ab498dd41
reonharuto <norabder567@gmail.com>
2025-05-07 21:18:13 +0900

    Add categories title

--- frontend/test/e2e/routing.test.js
+++ frontend/test/e2e/routing.test.js
@@ -137,6 +137,80 @@ describe('Users routing', () => {
   })
 })
 
+describe('Categories routing', () => {
+  it('「カテゴリーリスト」ページに遷移できること', async () => {
+    router.push('/categories')
+
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    await flushPromises()
+
+    expect(router.currentRoute.value.meta.title).toBe('Category Index')
+    expect(document.title).toBe('Category Index')
+    expect(wrapper.find('h3').text()).toBe('カテゴリーリスト')
+  })
+
+  it('「カテゴリー情報」ページに遷移できること', async () => {
+    router.push('/categories/1')
+
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    await flushPromises()
+
+    expect(router.currentRoute.value.meta.title).toBe('Category Show')
+    expect(document.title).toBe('Category Show')
+    expect(wrapper.find('h3').text()).toBe('カテゴリー情報')
+  })
+
+  it('「カテゴリー情報の登録」ページに遷移できること', async () => {
+    router.push('/categories/new')
+
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    await flushPromises()
+
+    expect(router.currentRoute.value.meta.title).toBe('Category New')
+    expect(document.title).toBe('Category New')
+    expect(wrapper.find('h3').text()).toBe('カテゴリー情報の登録')
+  })
+
+  it('「カテゴリー情報の編集」ページに遷移できること', async () => {
+    router.push('/categories/1/edit')
+
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    await flushPromises()
+
+    expect(router.currentRoute.value.meta.title).toBe('Category Edit')
+    expect(document.title).toBe('Category Edit')
+    expect(wrapper.find('h3').text()).toBe('カテゴリー情報の編集')
+  })
+})
+
 describe('Makers routing', () => {
   it('「メーカーリスト」ページに遷移できること', async () => {
     router.push('/makers')
