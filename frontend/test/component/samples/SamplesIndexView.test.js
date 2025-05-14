3d29fc400232376c374fbf18f469867f998a7266
reonharuto <norabder567@gmail.com>
2025-05-14 21:15:56 +0900

    Modify SamplesIndexView.vue

--- frontend/test/component/samples/SamplesIndexView.test.js
+++ frontend/test/component/samples/SamplesIndexView.test.js
@@ -1,15 +1,25 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
-import { mount, RouterLinkStub } from '@vue/test-utils'
+import { flushPromises, mount, RouterLinkStub } from '@vue/test-utils'
 import SamplesIndexView from '@/components/samples/SamplesIndexView.vue'
 import axios from 'axios'
 
 vi.mock('axios')
 
-vi.mock('vue-router', () => {
+const replaceMock = vi.fn()
+
+vi.mock('vue-router', async () => {
+  const actual = await vi.importActual('vue-router')
+
   return {
+    ...actual,
     useRoute: () => {
       return {
         query: { page: '1' }
+      }
+    },
+    useRouter: () => {
+      return {
+        replace: replaceMock
       }
     }
   }
@@ -18,116 +28,17 @@ vi.mock('vue-router', () => {
 describe('SamplesIndexView', () => {
   let wrapper
 
-  beforeEach(() => {
-    axios.get.mockResolvedValue({
-      data: {
-        samples: [
-          {
-            "id": 1,
-            "name": "無電解ニッケルめっき",
-            "category": "めっき",
-            "color": "イエローブラウンシルバー",
-            "maker": "小島印刷合同会社",
-            "created_at": "2025-02-23T22:15:29.815Z",
-            "updated_at": "2025-02-23T22:15:29.815Z",
-            "picture": "#<File:0x0000ffff859a8be0>",
-            "hardness": "析出状態の皮膜硬度でHV550～HV700、熱処理後の皮膜硬度はHV950程度",
-            "film_thickness": "通常は3～5μm、厚めの場合は20～50μmまで可能",
-            "feature": "耐食性・耐摩耗性・耐薬品性・耐熱性"
-          },
-          {
-            "id": 2,
-            "name": "白金めっき",
-            "category": "めっき",
-            "color": "シルバー",
-            "maker": "小山印刷合資会社",
-            "created_at": "2025-02-23T22:15:29.817Z",
-            "updated_at": "2025-02-23T22:15:29.817Z",
-            "picture": "#<File:0x0000ffff85969828>",
-            "hardness": "Hv300～Hv400程度",
-            "film_thickness": "水素水生成器用の白金電極では0.5～2.0μm、装飾品では0.1～0.5μm程度",
-            "feature": "耐蝕性・導電性・耐摩耗性・耐熱性"
-          },
-          {
-            "id": 3,
-            "name": "金めっき",
-            "category": "めっき",
-            "color": "ゴールド",
-            "maker": "合名会社木下銀行",
-            "created_at": "2025-02-23T22:15:29.818Z",
-            "updated_at": "2025-02-23T22:15:29.818Z",
-            "picture": "#<File:0x0000ffff85a1ab00>",
-            "hardness": "HV60～80程度",
-            "film_thickness": "下地ニッケルめっきは3～5μm、金めっきは0.1～1.0μm",
-            "feature": "耐食性・耐酸化性・電気抵抗性"
-          },
-          {
-            "id": 4,
-            "name": "銀めっき",
-            "category": "めっき",
-            "color": "シルバー",
-            "maker": "杉山食品有限会社",
-            "created_at": "2025-02-23T22:15:29.819Z",
-            "updated_at": "2025-02-23T22:15:29.819Z",
-            "picture": "#<File:0x0000ffff85aebde0>",
-            "hardness": "HV60～80程度",
-            "film_thickness": "0.1～3μm程度",
-            "feature": "耐摩耗性・潤滑性・耐食性・導電性"
-          },
-          {
-            "id": 5,
-            "name": "銅めっき",
-            "category": "めっき",
-            "color": "ブロンズ",
-            "maker": "金子保険合資会社",
-            "created_at": "2025-02-23T22:15:29.821Z",
-            "updated_at": "2025-02-23T22:15:29.821Z",
-            "picture": "#<File:0x0000ffff85b8cd30>",
-            "hardness": "無光沢でHv80～120、光沢でHv80～200程度",
-            "film_thickness": "0.2～2μm程度",
-            "feature": "抗菌性・密着性"
-          },
-          {
-            "id": 6,
-            "name": "亜鉛めっき",
-            "category": "めっき",
-            "color": "シルバー",
-            "maker": "中島水産合名会社",
-            "created_at": "2025-02-23T22:15:29.822Z",
-            "updated_at": "2025-02-23T22:15:29.822Z",
-            "picture": "#<File:0x0000ffff85c3da40>",
-            "hardness": "シアン浴でHv60～90、ジンケート浴でHv100～140、塩化浴でHv60～90",
-            "film_thickness": "5～20μm程度",
-            "feature": "耐食性・耐腐食性・密着性"
-          },
-          {
-            "id": 7,
-            "name": "錫めっき",
-            "category": "めっき",
-            "color": "ホワイトシルバー",
-            "maker": "合名会社今井情報",
-            "created_at": "2025-02-23T22:15:29.824Z",
-            "updated_at": "2025-02-23T22:15:29.824Z",
-            "picture": "#<File:0x0000ffff85cdea58>",
-            "hardness": "Hv9.5～10.5程度",
-            "film_thickness": "光沢スズめっきで3～10μm、無光沢スズめっきで5～20μm程度",
-            "feature": "耐食性・潤滑性・摺動性"
-          },
-        ],
-        curent_page: 1,
-        total_pages: 1
-      }
-    }),
-    wrapper = mount(SamplesIndexView, {
-      global: {
-        stubs: {
-          RouterLink: RouterLinkStub
-        }
-      }
-    })  
-  })
-
   describe('DOMの構造', () => {
+    beforeEach(() => {
+      wrapper = mount(SamplesIndexView, {
+        global: {
+          stubs: {
+            RouterLink: RouterLinkStub
+          }
+        }
+      })
+    })
+
     it('見出しが存在すること', () => {
       expect(wrapper.find('h3').text()).toBe('表面処理リスト')
     })
@@ -154,14 +65,68 @@ describe('SamplesIndexView', () => {
   })
 
   describe('API通信', () => {
-    it('表面処理リストが7件表示されること', () => {
-      expect(wrapper.html()).toContain('無電解ニッケルめっき')
-      expect(wrapper.html()).toContain('白金めっき')
-      expect(wrapper.html()).toContain('金めっき')
-      expect(wrapper.html()).toContain('銀めっき')
-      expect(wrapper.html()).toContain('銅めっき')
-      expect(wrapper.html()).toContain('亜鉛めっき')
-      expect(wrapper.html()).toContain('錫めっき')
+    describe('表面処理リストの取得に成功した場合', () => {
+      it('表面処理リストが7件表示されること', async () => {
+        axios.get.mockResolvedValue({
+          data: {
+            samples: [
+              { "id": 1, "name": "無電解ニッケルめっき" },
+              { "id": 2, "name": "白金めっき" },
+              { "id": 3, "name": "金めっき" },
+              { "id": 4, "name": "銀めっき" },
+              { "id": 5, "name": "銅めっき" },
+              { "id": 6, "name": "亜鉛めっき" },
+              { "id": 7, "name": "錫めっき" },
+            ],
+            curent_page: 1,
+            total_pages: 1
+          }
+        })
+
+        wrapper = mount(SamplesIndexView, {
+          global: {
+            stubs: {
+              RouterLink: RouterLinkStub
+            }
+          }
+        })
+
+        await flushPromises()
+
+        expect(wrapper.html()).toContain('無電解ニッケルめっき')
+        expect(wrapper.html()).toContain('白金めっき')
+        expect(wrapper.html()).toContain('金めっき')
+        expect(wrapper.html()).toContain('銀めっき')
+        expect(wrapper.html()).toContain('銅めっき')
+        expect(wrapper.html()).toContain('亜鉛めっき')
+        expect(wrapper.html()).toContain('錫めっき')
+      })
+    })
+
+    describe('表面処理リストの取得に失敗した場合', () => {
+      it('404 ページに遷移すること', async () => {
+        axios.get.mockRejectedValue({
+          response: {
+            status: 404
+          }
+        })
+
+        wrapper = mount(SamplesIndexView, {
+          global: {
+            stubs: {
+              RouterLink: RouterLinkStub
+            }
+          }
+        })
+
+        await flushPromises()
+
+        expect(wrapper.emitted()).toHaveProperty('message')
+        expect(wrapper.emitted().message[0]).toEqual([
+          { type: 'danger', text: '表面処理リストの取得に失敗しました。' }])
+
+        expect(replaceMock).toHaveBeenCalledWith({ name: 'NotFound' })
+      })
     })
   })
 })
