6be44e11c7e2134567683e3fd22138bdb385e536
reonharuto <norabder567@gmail.com>
2025-05-31 08:55:22 +0900

    Modify makers_controller.rb

--- backend/app/controllers/makers_controller.rb
+++ backend/app/controllers/makers_controller.rb
@@ -8,34 +8,34 @@ class MakersController < ApplicationController
   end
 
   def show
-    @maker = Maker.find(params[:id])
+    maker = Maker.find(params[:id])
 
-    render json: @maker
+    render json: maker
   end
 
   def create
-    @maker = Maker.new(maker_params)
+    maker = Maker.new(maker_params)
 
-    if @maker.save
-      render json: @maker, status: :created, location: @maker
+    if maker.save
+      render json: maker, status: :created, location: maker
     else
-      render json: @maker.errors, status: :unprocessable_entity
+      render json: maker.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    @maker = Maker.find(params[:id])
+    maker = Maker.find(params[:id])
 
-    if @maker.update(maker_params)
-      render json: @maker
+    if maker.update(maker_params)
+      render json: maker
     else
-      render json: @maker.errors, status: :unprocessable_entity
+      render json: maker.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @maker = Maker.find(params[:id])
-    @maker.destroy
+    maker = Maker.find(params[:id])
+    maker.destroy
   end
 
   private
