61dcb040e6b7b846059a17c9868c8e17e83cf777
reonharuto <norabder567@gmail.com>
2025-07-16 19:53:13 +0900

    Modify settings view test

--- frontend/test/components/settings/SettingsView.test.js
+++ frontend/test/components/settings/SettingsView.test.js
@@ -1,39 +1,78 @@
-import { mount } from '@vue/test-utils'
 import SettingsView from '@/components/settings/SettingsView.vue'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { flushPromises, mount, RouterLinkStub } from '@vue/test-utils'
 
-const context = describe
-
-describe('SettingsView.vue', () => {
+describe('SettingsView', () => {
   let wrapper
 
-  beforeEach(() => {
-    wrapper = mount(SettingsView, {
-      global: {
-        stubs: {
-          RouterLink: true
+  describe('初期レンダリング', () => {
+    beforeEach(async () => {
+      wrapper = mount(SettingsView, {
+        global: {
+          stubs: {
+            RouterLink: RouterLinkStub
+          }
         }
-      }
+      })
+
+      await flushPromises()
+    })
+
+    it('見出しが表示されること', () => {
+      expect(wrapper.find('h3').text()).toBe('アプリケーションの管理')
+    })
+
+    it('ログアウトボタンが表示されること', () => {
+      expect(wrapper.find('button').exists()).toBe(true)
+      expect(wrapper.find('h6').text()).toBe('ログアウト')
+      expect(wrapper.find('small').text()).toBe('アプリケーションからログアウトします。')
+    })
+
+    it('外部リンクが表示されること', () => {
+      const link = wrapper.findComponent(RouterLinkStub)
+
+      expect(link.props().to).toBe('/home')
+      expect(link.text()).toBe('メインメニューへ')
     })
   })
 
-  context('ログアウトの選択でOKを押した場合', () => {
+  describe('ログアウトの選択でOKを押した場合', () => {
     it('logoutイベントが発火すること', async () => {
       vi.spyOn(window, 'confirm').mockReturnValue(true)
     
+      wrapper = mount(SettingsView, {
+        global: {
+          stubs: {
+            RouterLink: RouterLinkStub
+          }
+        }
+      })
+
+      await flushPromises()
+
       await wrapper.find('button').trigger('click')
-      
-      expect(wrapper.emitted('logout')).toBeTruthy()
+
+      expect(wrapper.emitted()).toHaveProperty('logout')
     })
   })
 
-  context('ログアウトの選択でキャンセルを押した場合', () => {
+  describe('ログアウトの選択でキャンセルを押した場合', () => {
     it('logoutイベントが発火しないこと', async () => {
       vi.spyOn(window, 'confirm').mockReturnValue(false)
+
+      wrapper = mount(SettingsView, {
+        global: {
+          stubs: {
+            RouterLink: RouterLinkStub
+          }
+        }
+      })
+
+      await flushPromises()
     
       await wrapper.find('button').trigger('click')
       
-      expect(wrapper.emitted('logout')).toBeFalsy()
+      expect(wrapper.emitted()).not.toHaveProperty('logout')
     })
   })
 })
