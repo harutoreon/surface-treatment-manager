bb811cfafc131af7e55d0405886c702858f70884
reonharuto <norabder567@gmail.com>
2025-08-21 08:37:37 +0900

    sessions.js

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -6,14 +6,10 @@ import departmentRoutes from './routes/departments'
 import makerRoutes from './routes/makers'
 import sampleRoutes from './routes/samples'
 import userRoutes from './routes/users'
+import sessionRoutes from './routes/sessions'
 
-import LoginView from '@/components/sessions/LoginView.vue'
 import HomeView from '@/components/static_pages/HomeView.vue'
 import SettingsView from '@/components/settings/SettingsView.vue'
-// import UsersIndexView from '@/components/users/UsersIndexView.vue'
-// import UsersShowView from '@/components/users/UsersShowView.vue'
-// import UsersNewView from '@/components/users/UsersNewView.vue'
-// import UsersEditView from '@/components/users/UsersEditView.vue'
 import StaticPagesNameView from '@/components/static_pages/StaticPagesNameView.vue'
 import StaticPagesCategoryView from '@/components/static_pages/StaticPagesCategoryView.vue'
 import StaticPagesMakerView from '@/components/static_pages/StaticPagesMakerView.vue'
@@ -25,19 +21,15 @@ import NotFound from '@/components/not_found/NotFound.vue'
 const history = import.meta.env.MODE === 'test' ? createMemoryHistory() : createWebHistory()
 
 const routes = [
-  { path: '/', component: LoginView, meta: { title: 'Login' } },
   { path: '/home', component: HomeView, meta: { title: 'Home' } },
   { path: '/settings', component: SettingsView, meta: { title: 'Settings' }  },
-  // { path: '/users', component: UsersIndexView, meta: { title: 'User Index' } },
-  // { path: '/users/:id', component: UsersShowView, meta: { title: 'User Show' } },
-  // { path: '/users/new', component: UsersNewView, meta: { title: 'User New' } },
-  // { path: '/users/:id/edit', component: UsersEditView, meta: { title: 'User Edit' } },
   ...categoryRoutes,
   ...commentRoutes,
   ...departmentRoutes,
   ...makerRoutes,
   ...sampleRoutes,
   ...userRoutes,
+  ...sessionRoutes,
   { path: '/static_pages/name', component: StaticPagesNameView, meta: { title: 'Static Pages Name' } },
   { path: '/static_pages/category', component: StaticPagesCategoryView, meta: { title: 'Static Pages Category' } },
   { path: '/static_pages/maker', component: StaticPagesMakerView, meta: { title: 'Static Pages Maker' } },
