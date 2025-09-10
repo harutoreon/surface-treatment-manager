b7d10aaa388647f318aa48d662af2795f38eacce
reonharuto <norabder567@gmail.com>
2025-09-10 19:38:25 +0900

    Centralization of Session Settings

---
+++ backend/config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+# config/initializers/session_store.rb
+Rails.application.config.session_store :cookie_store,
+  key: '_surface_treatment_manager_session',
+  same_site: :none,
+  secure: Rails.env.production?
