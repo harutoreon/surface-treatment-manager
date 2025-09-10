575157677b6a6fe37dc59455d3d4ad4255c70529
reonharuto <norabder567@gmail.com>
2025-09-10 20:27:17 +0900

    Fix session_store.rb and routes.rb

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -3,3 +3,5 @@ Rails.application.config.session_store :cookie_store,
   key: '_surface_treatment_manager_session',
   same_site: :none,
   secure: true
+
+  Rails.logger.info "=== Session options: #{Rails.application.config.session_options.inspect} ==="
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -23,4 +23,10 @@ Rails.application.routes.draw do
   resources :samples do
     resources :comments
   end
+
+  Rails.application.routes.draw do
+    get "/debug_session", to: ->(env) {
+      [200, { "Content-Type" => "application/json" }, [ Rails.application.config.session_options.to_json ]]
+    }
+  end
 end
