bdf01881f8d98a4d8b0d4ff16a94cfd73c186805
reonharuto <norabder567@gmail.com>
2025-09-13 18:15:47 +0900

    fix

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -2,7 +2,8 @@
 Rails.application.config.session_store :cookie_store,
   key: '_surface_treatment_manager_session',
   same_site: :none,
-  secure: true
+  # secure: true
+  secure: Rails.env.production?
 
 # セッション設定をログに出力する
 # Rails.logger.info "=== Session options: #{Rails.application.config.session_options.inspect} ==="
