dd73f9aef28a03889137144f83b59230c5dd6409
reonharuto <norabder567@gmail.com>
2025-09-13 11:32:22 +0900

    Fix session_store.rb

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # config/initializers/session_store.rb
-# Rails.application.config.session_store :cookie_store,
-#   key: '_surface_treatment_manager_session',
-#   same_site: :none,
-#   secure: true
+Rails.application.config.session_store :cookie_store,
+  key: '_surface_treatment_manager_session',
+  same_site: :none,
+  secure: true
 
-  # セッション設定をログに出力する
-  # Rails.logger.info "=== Session options: #{Rails.application.config.session_options.inspect} ==="
+# セッション設定をログに出力する
+Rails.logger.info "=== Session options: #{Rails.application.config.session_options.inspect} ==="
