2aa5093c0cb92020139671bd441bf2222bbee6a7
reonharuto <norabder567@gmail.com>
2025-10-15 21:09:15 +0900

    Add CardComponent.vue

---
+++ frontend/src/components/static_pages/CardComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  id: String,
+  alt: String,
+  icon: String,
+  cardTitle: String,
+  cardText: String,
+  toAttribute: String,
+  linkText: String
+})
+</script>
+
+<template>
+  <div class="card pt-2" v-bind:id="props.id" style="width: 19rem;">
+    <img
+      v-bind:alt="props.alt"
+      class="card-img-top w-25"
+      width="50"
+      height="50"
+      v-bind:src="props.icon"
+    >
+    <div class="card-body">
+      <h5 class="card-title">
+        {{ props.cardTitle }}
+      </h5>
+      <p class="card-text">
+        {{ props.cardText }}
+      </p>
+      <RouterLink v-bind:to="props.toAttribute" class="card-link">
+        {{ props.linkText }}
+      </RouterLink>
+    </div>
+  </div>
+</template>
