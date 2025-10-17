26e6b8b77d4a83c6e9ebfe492e0a62ceecad3c29
reonharuto <norabder567@gmail.com>
2025-10-18 03:29:21 +0900

    Remove import statements

--- frontend/test/components/static_pages/HomeView.test.js
+++ frontend/test/components/static_pages/HomeView.test.js
@@ -1,17 +1,6 @@
 import HomeView from '@/components/static_pages/HomeView.vue'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { flushPromises, mount, RouterLinkStub } from '@vue/test-utils'
-import experimentIcon from '@/assets/icons/experiment.svg'
-import categoryIcon from '@/assets/icons/category.svg'
-import factoryIcon from '@/assets/icons/factory.svg'
-import listIcon from '@/assets/icons/list.svg'
-import libraryAddIcon from '@/assets/icons/library_add.svg'
-import categoryAddIcon from '@/assets/icons/category_add.svg'
-import makerAddIcon from '@/assets/icons/maker_add.svg'
-import userAddIcon from '@/assets/icons/user_add.svg'
-import departmentIcon from '@/assets/icons/department.svg'
-import commentIcon from '@/assets/icons/comment.svg'
-import settingsIcon from '@/assets/icons/settings.svg'
 import axios from 'axios'
 
 vi.mock('axios')
@@ -47,7 +36,7 @@ describe('HomeView', () => {
       const divManageSettings = wrapper.find('#manage-settings')
       const routerLink = divManageSettings.findComponent(RouterLinkStub)
 
-      expect(divManageSettings.find('img').attributes('src')).toBe(settingsIcon)
+      expect(divManageSettings.find('img').exists()).toBe(true)
       expect(divManageSettings.find('img').attributes('alt')).toBe('settings icon')
       expect(divManageSettings.find('div h5').text()).toBe('アプリケーションの管理')
       expect(divManageSettings.find('div p').text()).toBe('アプリケーションの設定やログアウトを行います。')
@@ -79,7 +68,7 @@ describe('HomeView', () => {
       const divSearchName = wrapper.find('#search-name')
       const routerLink = divSearchName.findComponent(RouterLinkStub)
 
-      expect(divSearchName.find('img').attributes('src')).toBe(experimentIcon)
+      expect(divSearchName.find('img').exists()).toBe(true)
       expect(divSearchName.find('img').attributes('alt')).toBe('experiment icon')
       expect(divSearchName.find('div h5').text()).toBe('処理名で検索')
       expect(divSearchName.find('div p').text()).toBe('処理名を入力して表面処理を検索します。')
@@ -91,7 +80,7 @@ describe('HomeView', () => {
       const divSearchCategory = wrapper.find('#search-category')
       const routerLink = divSearchCategory.findComponent(RouterLinkStub)
 
-      expect(divSearchCategory.find('img').attributes('src')).toBe(categoryIcon)
+      expect(divSearchCategory.find('img').exists()).toBe(true)
       expect(divSearchCategory.find('img').attributes('alt')).toBe('category icon')
       expect(divSearchCategory.find('div h5').text()).toBe('カテゴリーで検索')
       expect(divSearchCategory.find('div p').text()).toBe('カテゴリーを選択して表面処理を検索します。')
@@ -103,7 +92,7 @@ describe('HomeView', () => {
       const divSearchMaker = wrapper.find('#search-maker')
       const routerLink = divSearchMaker.findComponent(RouterLinkStub)
 
-      expect(divSearchMaker.find('img').attributes('src')).toBe(factoryIcon)
+      expect(divSearchMaker.find('img').exists()).toBe(true)
       expect(divSearchMaker.find('img').attributes('alt')).toBe('factory icon')
       expect(divSearchMaker.find('div h5').text()).toBe('メーカー名で検索')
       expect(divSearchMaker.find('div p').text()).toBe('メーカー名を入力して表面処理を検索します。')
@@ -115,7 +104,7 @@ describe('HomeView', () => {
       const divSearchList = wrapper.find('#search-list')
       const routerLink = divSearchList.findComponent(RouterLinkStub)
 
-      expect(divSearchList.find('img').attributes('src')).toBe(listIcon)
+      expect(divSearchList.find('img').exists()).toBe(true)
       expect(divSearchList.find('img').attributes('alt')).toBe('list icon')
       expect(divSearchList.find('div h5').text()).toBe('処理一覧から検索')
       expect(divSearchList.find('div p').text()).toBe('表面処理一覧から目的の処理を検索します。')
@@ -147,7 +136,7 @@ describe('HomeView', () => {
       const divManageSamples = wrapper.find('#manage-samples')
       const routerLink = divManageSamples.findComponent(RouterLinkStub)
 
-      expect(divManageSamples.find('img').attributes('src')).toBe(libraryAddIcon)
+      expect(divManageSamples.find('img').exists()).toBe(true)
       expect(divManageSamples.find('img').attributes('alt')).toBe('library add icon')
       expect(divManageSamples.find('div h5').text()).toBe('表面処理の管理')
       expect(divManageSamples.find('div p').text()).toBe('表面処理に関する情報を一括管理します。')
@@ -159,7 +148,7 @@ describe('HomeView', () => {
       const divManageCategories = wrapper.find('#manage-categories')
       const routerLink = divManageCategories.findComponent(RouterLinkStub)
 
-      expect(divManageCategories.find('img').attributes('src')).toBe(categoryAddIcon)
+      expect(divManageCategories.find('img').exists()).toBe(true)
       expect(divManageCategories.find('img').attributes('alt')).toBe('category add icon')
       expect(divManageCategories.find('div h5').text()).toBe('カテゴリーの管理')
       expect(divManageCategories.find('div p').text()).toBe('カテゴリーに関する情報を一括管理します。')
@@ -171,7 +160,7 @@ describe('HomeView', () => {
       const divManageMakers = wrapper.find('#manage-makers')
       const routerLink = divManageMakers.findComponent(RouterLinkStub)
 
-      expect(divManageMakers.find('img').attributes('src')).toBe(makerAddIcon)
+      expect(divManageMakers.find('img').exists()).toBe(true)
       expect(divManageMakers.find('img').attributes('alt')).toBe('maker add icon')
       expect(divManageMakers.find('div h5').text()).toBe('メーカーの管理')
       expect(divManageMakers.find('div p').text()).toBe('メーカーに関する情報を一括管理します。')
@@ -183,7 +172,7 @@ describe('HomeView', () => {
       const divManageUsers = wrapper.find('#manage-users')
       const routerLink = divManageUsers.findComponent(RouterLinkStub)
 
-      expect(divManageUsers.find('img').attributes('src')).toBe(userAddIcon)
+      expect(divManageUsers.find('img').exists()).toBe(true)
       expect(divManageUsers.find('img').attributes('alt')).toBe('user add icon')
       expect(divManageUsers.find('div h5').text()).toBe('ユーザーの管理')
       expect(divManageUsers.find('div p').text()).toBe('ユーザーに関する情報を一括管理します。')
@@ -195,7 +184,7 @@ describe('HomeView', () => {
       const divManageDepartments = wrapper.find('#manage-departments')
       const routerLink = divManageDepartments.findComponent(RouterLinkStub)
 
-      expect(divManageDepartments.find('img').attributes('src')).toBe(departmentIcon)
+      expect(divManageDepartments.find('img').exists()).toBe(true)
       expect(divManageDepartments.find('img').attributes('alt')).toBe('department add icon')
       expect(divManageDepartments.find('div h5').text()).toBe('部署の管理')
       expect(divManageDepartments.find('div p').text()).toBe('部署に関する情報を一括管理します。')
@@ -207,7 +196,7 @@ describe('HomeView', () => {
       const divManageComments = wrapper.find('#manage-comments')
       const routerLink = divManageComments.findComponent(RouterLinkStub)
 
-      expect(divManageComments.find('img').attributes('src')).toBe(commentIcon)
+      expect(divManageComments.find('img').exists()).toBe(true)
       expect(divManageComments.find('img').attributes('alt')).toBe('comment icon')
       expect(divManageComments.find('div h5').text()).toBe('コメントの管理')
       expect(divManageComments.find('div p').text()).toBe('コメントに関する情報を一括管理します。')
