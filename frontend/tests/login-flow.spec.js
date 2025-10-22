7d596916dafdb98b7a69b1e19d099b5f768c8bc5
reonharuto <norabder567@gmail.com>
2025-10-23 04:13:22 +0900

    Add cases where you log in by entering valid information

--- frontend/tests/login-flow.spec.js
+++ frontend/tests/login-flow.spec.js
@@ -16,6 +16,7 @@ test.describe('一般ユーザーでログインした場合', () => {
     await expect(page.getByRole('heading', { name: '処理一覧から検索' })).toBeVisible()
     await expect(page.getByRole('heading', { name: 'アプリケーションの管理' })).toBeVisible()
 
+    await expect(page.getByText('ログインしました。')).toBeVisible()
     await page.getByRole('button').click()  // 通知を閉じる
   })
 })
@@ -29,6 +30,7 @@ test.describe('管理者ユーザーでログインした場合', () => {
     await page.getByRole('button', { name: 'ログイン' }).click()
 
     await expect(page).toHaveURL('http://localhost:5173/home')
+    await expect(page.getByRole('heading', { name: 'メインメニュー' })).toBeVisible()
     await expect(page.getByRole('heading', { name: '表面処理の管理' })).toBeVisible()
     await expect(page.getByRole('heading', { name: 'カテゴリーの管理' })).toBeVisible()
     await expect(page.getByRole('heading', { name: 'メーカーの管理' })).toBeVisible()
@@ -37,6 +39,24 @@ test.describe('管理者ユーザーでログインした場合', () => {
     await expect(page.getByRole('heading', { name: 'コメントの管理' })).toBeVisible()
     await expect(page.getByRole('heading', { name: 'アプリケーションの管理' })).toBeVisible()
 
+    await expect(page.getByText('ログインしました。')).toBeVisible()
     await page.getByRole('button').click()  // 通知を閉じる
+  })
+})
+
+test.describe('有効なユーザー名とパスワードを入力してログインした場合', () => {
+  test('ログインに成功すること', async ({ page }) => {
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByRole('heading', { name: 'ログイン' })).toBeVisible()
+
+    await page.getByRole('textbox', { name: 'ユーザー名' }).fill('general user')
+    await page.getByRole('textbox', { name: 'パスワード' }).fill('generalpassword')
+    await page.getByRole('button', { name: 'ログイン' }).click()
+
+    await expect(page).toHaveURL('http://localhost:5173/home')
+    await expect(page.getByRole('heading', { name: 'メインメニュー' })).toBeVisible()
+
+    await expect(page.getByText('ログインしました。')).toBeVisible()
+    await page.getByRole('button').click()
   })
 })
