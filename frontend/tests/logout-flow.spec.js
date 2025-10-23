171c3cfa51a29a6ee3dd9ebd7ab2c1b924a3d0c8
reonharuto <norabder567@gmail.com>
2025-10-23 20:55:49 +0900

    Add logout flow

---
+++ frontend/tests/logout-flow.spec.js
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('ログアウトフロー', () => {
+  test.describe('ログアウトのアラートでOKを押した場合', () => {
+    test('ログインページに移動すること', async ({ page }) => {
+      await page.goto('http://localhost:5173')
+
+      await page.getByRole('radio', { name: '一般ユーザー' }).check()
+      await page.getByRole('button', { name: 'ログイン' }).click()
+
+      await page.getByRole('button', { name: '通知を閉じる' }).click()
+
+      await page.getByRole('link', { name: '管理ページへ' }).click()
+      await expect(page).toHaveURL('http://localhost:5173/settings')
+      await expect(page.getByRole('heading', { name: 'アプリケーションの管理' })).toBeVisible()
+
+      page.once('dialog', async dialog => {
+        await dialog.accept()
+      })
+
+      await page.getByRole('button', { name: 'ログアウト' }).click()
+
+      await expect(page).toHaveURL('http://localhost:5173')
+      await expect(page.getByRole('heading', { name: 'ログイン' })).toBeVisible()
+    })
+  })
+
+  test.describe('ログアウトのアラートでキャンセルを押した場合', () => {
+    test('アプリケーションの管理ページに留まること', async ({ page }) => {
+      await page.goto('http://localhost:5173')
+
+      await page.getByRole('radio', { name: '一般ユーザー' }).check()
+      await page.getByRole('button', { name: 'ログイン' }).click()
+
+      await page.getByRole('button', { name: '通知を閉じる' }).click()
+
+      await page.getByRole('link', { name: '管理ページへ' }).click()
+      await expect(page).toHaveURL('http://localhost:5173/settings')
+      await expect(page.getByRole('heading', { name: 'アプリケーションの管理' })).toBeVisible()
+
+      page.once('dialog', async dialog => {
+        await dialog.dismiss()
+      })
+
+      await page.getByRole('button', { name: 'ログアウト' }).click()
+
+      await expect(page).toHaveURL('http://localhost:5173/settings')
+      await expect(page.getByRole('heading', { name: 'アプリケーションの管理' })).toBeVisible()
+    })
+  })
+})
