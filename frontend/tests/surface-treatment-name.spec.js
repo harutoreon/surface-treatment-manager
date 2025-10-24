c40ff0a84f42c0de238a68c13eff3f2ef962d03d
reonharuto <norabder567@gmail.com>
2025-10-25 07:06:00 +0900

    Add a flow to search for invalid strings

--- frontend/tests/surface-treatment-name.spec.js
+++ frontend/tests/surface-treatment-name.spec.js
@@ -27,4 +27,20 @@ test.describe('表面処理名で検索', () => {
       await expect(page.getByRole('link', { name: '錫めっき' })).toBeVisible()
     })
   })
+
+  test.describe('検索文字列「無効な検索文字列」で検索した場合', () => {
+    test('該当する表面処理がないメッセージが表示されること', async ({ page }) => {
+      await expect(page).toHaveURL('http://localhost:5173/static_pages/name')
+      await expect(page.getByRole('heading', { name: '処理名で検索' })).toBeVisible()
+
+      await page.getByRole('textbox', { name: 'キーワードをここに入力' }).fill('無効な検索文字列')
+      await page.getByRole('button', { name: '検索' }).click()
+
+      await expect(page).toHaveURL(
+        'http://localhost:5173/static_pages/name/search_results?keyword=無効な検索文字列'
+      )
+      await expect(page.getByRole('heading', { name: '表面処理の検索結果' })).toBeVisible()
+      await expect(page.getByRole('heading', { name: '該当する表面処理はありませんでした。' })).toBeVisible()
+    })
+  })
 })
