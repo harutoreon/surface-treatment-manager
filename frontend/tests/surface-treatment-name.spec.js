f58c8eabe3a5dc33704a7911917102fb7927fe3b
reonharuto <norabder567@gmail.com>
2025-10-25 06:02:27 +0900

    Add a flow to search using valid strings

---
+++ frontend/tests/surface-treatment-name.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('表面処理名で検索', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('radio', { name: '一般ユーザー' }).check()
+    await page.getByRole('button', { name: 'ログイン' }).click()
+
+    await page.getByRole('button', { name: '通知を閉じる' }).click()
+
+    await page.locator('#search-name').getByRole('link', { name: '検索ページへ' }).click()
+  })
+
+  test.describe('検索文字列「めっき」で検索した場合', () => {
+    test('文字列「めっき」を含む表面処理がヒットすること', async ({ page }) => {
+      await expect(page).toHaveURL('http://localhost:5173/static_pages/name')
+      await expect(page.getByRole('heading', { name: '処理名で検索' })).toBeVisible()
+
+      await page.getByRole('textbox', { name: 'キーワードをここに入力' }).fill('めっき')
+      await page.getByRole('button', { name: '検索' }).click()
+
+      await expect(page).toHaveURL(
+        'http://localhost:5173/static_pages/name/search_results?keyword=めっき'
+      )
+      await expect(page.getByRole('heading', { name: '表面処理の検索結果' })).toBeVisible()
+      await expect(page.getByRole('link', { name: '錫めっき' })).toBeVisible()
+    })
+  })
+})
