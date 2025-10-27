ea9126f2185507f7b4ef736423ec73ab6ea80dcc
reonharuto <norabder567@gmail.com>
2025-10-27 19:50:07 +0900

    Add link operation to samples-index.spec.js

--- frontend/tests/resources-manage-flow/samples/samples-index.spec.js
+++ frontend/tests/resources-manage-flow/samples/samples-index.spec.js
@@ -7,23 +7,43 @@ test.describe('samples flow', () => {
     await page.getByRole('button', { name: 'ログイン' }).click()
     await page.getByRole('button', { name: '通知を閉じる' }).click()
 
-    await page.locator('#manage-samples').getByRole('link', { name: '管理ページへ' }).click()
+    await page.goto('/samples')
   })
 
-  test('表面処理リストページが表示されること', async ({ page }) => {
-    await expect(page).toHaveURL('/samples')
-    await expect(page.getByRole('heading', { name: '表面処理リスト' })).toBeVisible()
+  test.describe('初期レンダリングに成功した場合', () => {
+    test('表面処理リストが表示されること', async ({ page }) => {
+      await expect(page).toHaveURL('/samples')
+      await expect(page.getByRole('heading', { name: '表面処理リスト' })).toBeVisible()
 
-    await expect(page.getByRole('link', { name: '無電解ニッケルめっき' })).toBeVisible()
-    await expect(page.getByRole('link', { name: '白金めっき' })).toBeVisible()
-    await expect(page.getByRole('link', { name: '銀めっき' })).toBeVisible()
-    await expect(page.getByRole('link', { name: '錫めっき' })).toBeVisible()
+      await expect(page.getByRole('link', { name: '無電解ニッケルめっき' })).toBeVisible()
+      await expect(page.getByRole('link', { name: '白金めっき' })).toBeVisible()
+      await expect(page.getByRole('link', { name: '銀めっき' })).toBeVisible()
+      await expect(page.getByRole('link', { name: '錫めっき' })).toBeVisible()
 
-    await expect(page.locator('#pagination_previous_page')).toBeVisible()
-    await expect(page.getByRole('link', { name: '1' })).toBeVisible()
-    await expect(page.getByRole('link', { name: '次ページ' })).toBeVisible()
+      await expect(page.locator('#pagination_previous_page')).toBeVisible()
+      await expect(page.getByRole('link', { name: '1' })).toBeVisible()
+      await expect(page.getByRole('link', { name: '次ページ' })).toBeVisible()
 
-    await expect(page.getByRole('link', { name: '表面処理情報の登録' })).toBeVisible()
-    await expect(page.getByRole('link', { name: 'メインメニューへ' })).toBeVisible()
+      await expect(page.getByRole('link', { name: '表面処理情報の登録' })).toBeVisible()
+      await expect(page.getByRole('link', { name: 'メインメニューへ' })).toBeVisible()
+    })
+  })
+
+  test.describe('表面処理情報の登録リンクをクリックした場合', () => {
+    test('/samples/newへ移動すること', async ({ page }) => {
+      await page.getByRole('link', { name: '表面処理情報の登録' }).click()
+
+      await expect(page).toHaveURL('/samples/new')
+      await expect(page.getByRole('heading', { name: '表面処理情報の登録' })).toBeVisible()
+    })
+  })
+
+  test.describe('メインメニューへのリンクをクリックした場合', () => {
+    test('/homeに移動すること', async ({ page }) => {
+      await page.getByRole('link', { name: 'メインメニューへ' }).click()
+
+      await expect(page).toHaveURL('/home')
+      await expect(page.getByRole('heading', { name: 'メインメニュー' })).toBeVisible()
+    })
   })
 })
