3f3e21331f8018fc5216451cda2680479acc8854
reonharuto <norabder567@gmail.com>
2025-10-28 22:03:28 +0900

    Add link operation to samples-new.spec.js

--- frontend/tests/resources-manage-flow/samples/samples-new.spec.js
+++ frontend/tests/resources-manage-flow/samples/samples-new.spec.js
@@ -6,11 +6,12 @@ test.describe('samples new flow', () => {
     await page.getByRole('radio', { name: '管理者ユーザー' }).check()
     await page.getByRole('button', { name: 'ログイン' }).click()
     await page.getByRole('button', { name: '通知を閉じる' }).click()
+
+    await page.goto('/samples/new')
   })
 
-  test.describe('/samples/newをリクエストした場合', () => {
+  test.describe('初期レンダリングに成功した場合', () => {
     test('表面処理情報の登録ページが表示されること', async ({ page }) => {
-      await page.goto('/samples/new')
       await expect(page.getByRole('heading', { name: '表面処理情報の登録' })).toBeVisible()
 
       await expect(page.locator('#sample-name')).toBeVisible()
@@ -26,6 +27,15 @@ test.describe('samples new flow', () => {
       await expect(page.getByRole('button', { name: '登録' })).toBeVisible()
 
       await expect(page.getByRole('link', { name: '表面処理リストへ' })).toBeVisible()
+    })
+  })
+
+  test.describe('表面処理リストへのリンクをクリックした場合', () => {
+    test('/samplesへ移動すること', async ({ page }) => {
+      await page.getByRole('link', { name: '表面処理リスト' }).click()
+
+      await expect(page).toHaveURL('/samples')
+      await expect(page.getByRole('heading', { name: '表面処理リスト' })).toBeVisible()
     })
   })
 })
