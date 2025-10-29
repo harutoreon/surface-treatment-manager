1c38565b2b29c899fbb454af49af259dd113f70b
reonharuto <norabder567@gmail.com>
2025-10-29 20:30:21 +0900

    Add categories-show.spec.js

---
+++ frontend/tests/resources-manage-flow/categories/categories-show.spec.js
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('categories show flow', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+    await page.getByRole('radio', { name: '管理者ユーザー' }).check()
+    await page.getByRole('button', { name: 'ログイン' }).click()
+    await page.getByRole('button', { name: '通知を閉じる' }).click()
+
+    await page.goto('/categories/1')
+  })
+
+  test.describe('初期レンダリングに成功した場合', () => {
+    test('カテゴリー情報が表示されること', async ({ page }) => {
+      await expect(page).toHaveURL('/categories/1')
+      await expect(page.getByRole('heading', { name: 'カテゴリー情報' })).toBeVisible()
+
+      await expect(page.getByRole('listitem')).toHaveText(
+        ['カテゴリー名 :めっき', '概要 :金属または非金属の材料の表面に金属の薄膜を被覆する処理のこと。']
+      )
+    })
+  })
+
+  test.describe('カテゴリーリストへのリンクをクリックした場合', () => {
+    test('/categoriesに移動すること', async ({ page }) => {
+      await page.getByRole('link', { name: 'カテゴリーリストへ' }).click()
+
+      await expect(page).toHaveURL('/categories')
+      await expect(page.getByRole('heading', { name: 'カテゴリーリスト' })).toBeVisible()
+    })
+  })
+})
