7795304781404f83831b68d54f720cda0a8466c5
reonharuto <norabder567@gmail.com>
2025-11-02 11:12:14 +0900

    Add comments-edit.spec.js

---
+++ frontend/tests/resources-manage-flow/comments/comments-edit.spec.js
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('comments edit flow', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+    await page.getByRole('radio', { name: '管理者ユーザー' }).check()
+    await page.getByRole('button', { name: 'ログイン' }).click()
+    await page.getByRole('button', { name: '通知を閉じる' }).click()
+
+    await page.goto('/comments/1/edit')
+  })
+
+  test.describe('初期レンダリングに成功した場合', () => {
+    test('コメント情報の編集ページが表示されること', async ({ page }) => {
+      await expect(page.getByRole('heading', { name: 'コメント情報の編集' })).toBeVisible()
+
+      await expect(page.locator('#department')).toBeVisible()
+      await expect(page.locator('#commenter')).toBeVisible()
+      await expect(page.locator('#body')).toBeVisible()
+
+      await expect(page.getByRole('button', { name: '更新' })).toBeVisible()
+
+      await expect(page.getByRole('link', { name: 'コメント情報へ' })).toBeVisible()
+      await expect(page.getByRole('link', { name: 'コメントリストへ' })).toBeVisible()
+    })
+  })
+
+  test.describe('コメントリストへのリンクをクリックした時', () => {
+    test('/commentsに移動すること', async ({ page }) => {
+      await page.getByRole('link', { name: 'コメントリストへ' }).click()
+
+      await expect(page).toHaveURL('/comments')
+      await expect(page.getByRole('heading', { name: 'コメントリスト' })).toBeVisible()
+    })
+  })
+})
