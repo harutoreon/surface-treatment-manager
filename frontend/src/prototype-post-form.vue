ef52766983c008ebe3137e87f8e445fc304f4baf
reonharuto <norabder567@gmail.com>
2025-11-16 09:12:48 +0900

    Add handleCommentAdd

--- frontend/src/prototype-post-form.vue
+++ frontend/src/prototype-post-form.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const department = ref('')
+const commenter = ref('')
+const commentInput = ref('')
+const comment = ref('')
+const createdAt = ref('')
+
+const handleCommentAdd = () => {
+  department.value = '営業部'
+  commenter.value = '佐藤 太郎'
+  comment.value = commentInput.value
+  createdAt.value = new Date().toISOString().slice(0, 10)
+}
+</script>
+
 <template>
   <button
     type="button"
@@ -7,6 +24,21 @@
   >
     コメントの新規作成
   </button>
+
+  <p
+    class="mt-5"
+  >
+    部署: {{ department }}
+  </p>
+  <p>
+    投稿者: {{ commenter }}
+  </p>
+  <p>
+    コメント: {{ comment }}
+  </p>
+  <p>
+    投稿日: {{ createdAt }}
+  </p>
 
   <div
     class="modal fade"
@@ -29,6 +61,7 @@
         </div>
         <div class="modal-body">
           <textarea
+            v-model="commentInput"
             class="form-control"
             id="comment-post"
             placeholder="コメントはここに入力して下さい。"></textarea>
@@ -44,6 +77,8 @@
           <button
             type="button"
             class="btn btn-primary"
+            data-bs-dismiss="modal"
+            v-on:click="handleCommentAdd"
           >
             リストに追加
           </button>
