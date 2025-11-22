dc30078c5c681eb9015d3ff93819f6e4ca66862f
reonharuto <norabder567@gmail.com>
2025-11-23 08:44:48 +0900

    Change layout

--- frontend/src/components/categories/CategoriesEditView.vue
+++ frontend/src/components/categories/CategoriesEditView.vue
@@ -75,23 +75,19 @@ onMounted(async () => {
         required
       >
       </textarea>
-      
-      <button type="submit" class="form-control btn btn-primary mb-5">
-        更新
-      </button>
+
+      <div class="d-grid gap-2 d-md-block">
+        <button type="submit" class="btn btn-primary me-md-2">
+          更新
+        </button>
+        <button type="button" class="btn btn-outline-secondary">
+          キャンセル
+        </button>
+      </div>
     </form>
 
     <p v-if="errorMessage" class="alert alert-danger mt-4" role="alert">
       {{ errorMessage }}
     </p>
-
-    <div class="d-flex justify-content-evenly">
-      <RouterLink v-if="category.id" v-bind:to="`/categories/${category.id}`" ref="linkCategoriesShow">
-        カテゴリー情報へ
-      </RouterLink>
-      <RouterLink to="/categories" ref="linkCategories">
-        カテゴリーリストへ
-      </RouterLink>
-    </div>
   </div>
 </template>
