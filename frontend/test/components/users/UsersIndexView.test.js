a96bc5181f22316ca0c805e48ee9234a582dd77b
reonharuto <norabder567@gmail.com>
2025-12-05 19:42:22 +0900

    Modify users index view

--- frontend/test/components/users/UsersIndexView.test.js
+++ frontend/test/components/users/UsersIndexView.test.js
@@ -144,8 +144,10 @@ describe('UsersIndexView', () => {
     })
 
     it('外部リンクが表示されること', () => {
-      const div = wrapper.find('div[class="d-flex justify-content-evenly"]')
-      const routerLinks = div.findAllComponents(RouterLinkStub)
+      // const div = wrapper.find('div[class="d-flex justify-content-evenly"]')
+
+      const ulElements = wrapper.findAll('ul')
+      const routerLinks = ulElements[1].findAllComponents(RouterLinkStub)
 
       // to属性
       expect(routerLinks[0].props().to).toBe('/users/new')
