a4201866bf5ed657a22ae55e5c2f839447d60d46
reonharuto <norabder567@gmail.com>
2026-03-21 05:49:43 +0900

    Modify category_spec.rb and department_spec.rb

--- backend/spec/models/category_spec.rb
+++ backend/spec/models/category_spec.rb
@@ -3,48 +3,51 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   let(:category) { FactoryBot.build(:thermal_spraying) }
 
-  describe '有効性の検証' do
-    it 'オブジェクトが有効であること' do
-      expect(category).to be_valid
-    end
-  end
-
-  describe '存在性の検証' do
-    it 'itemが空文字だと無効であること' do
-      category.item = ''
-      expect(category).to be_invalid
-      expect(category.errors[:item]).to include('（カテゴリー名）が空白です。')
-    end
-
-    it 'summaryが空文字だと無効であること' do
-      category.summary = ''
-      expect(category).to be_invalid
-      expect(category.errors[:summary]).to include('（概要）が空白です。')
-    end
-  end
-
-  describe '長さの検証' do
-    context '文字数が60文字以下の場合' do
-      it '有効であること' do
-        category.summary = 's' * 60
+  describe 'バリデーション' do
+    describe '有効性の検証' do
+      it 'オブジェクトが有効であること' do
         expect(category).to be_valid
       end
     end
 
-    context '文字数が60文字を超える場合' do
-      it '無効であること' do
-        category.summary = 's' * 61
-        expect(category).to be_invalid
-        expect(category.errors[:summary]).to include('（概要）が60文字を超えています。')
+    describe '存在性の検証' do
+      it 'itemが空文字だと無効であること' do
+        category.item = ''
+        category.valid?
+        expect(category.errors.details[:item]).to include(error: :blank)
+      end
+
+      it 'summaryが空文字だと無効であること' do
+        category.summary = ''
+        category.valid?
+        expect(category.errors.details[:summary]).to include(error: :blank)
       end
     end
-  end
 
-  describe '一意性の検証' do
-    it 'itemが重複した場合、無効であること' do
-      category.save
-      duplicate = FactoryBot.build(:thermal_spraying, item: category.item)
-      expect(duplicate).to be_invalid
+    describe '長さの検証' do
+      context 'summaryの文字数が60文字以内の場合' do
+        it '有効であること' do
+          category.summary = 's' * 60
+          expect(category).to be_valid
+        end
+      end
+
+      context 'summaryの文字数が60文字を超える場合' do
+        it '無効であること' do
+          category.summary = 's' * 61
+          category.valid?
+          expect(category.errors.details[:summary].first).to include(error: :too_long)
+        end
+      end
+    end
+
+    describe '一意性の検証' do
+      it 'itemが重複した場合、無効であること' do
+        category.save
+        duplicate = FactoryBot.build(:thermal_spraying, item: category.item)
+        duplicate.valid?
+        expect(duplicate.errors.details[:item].first).to include(error: :taken)
+      end
     end
   end
 end
--- backend/spec/models/department_spec.rb
+++ backend/spec/models/department_spec.rb
@@ -3,26 +3,28 @@ require 'rails_helper'
 RSpec.describe Department, type: :model do
   let(:department) { FactoryBot.build(:department, name: '生産管理部') }
 
-  describe '有効性の検証' do
-    it 'オブジェクトが有効であること' do
-      expect(department).to be_valid
+  describe 'バリデーション' do
+    describe '有効性の検証' do
+      it 'オブジェクトが有効であること' do
+        expect(department).to be_valid
+      end
     end
-  end
 
-  describe '存在性の検証' do
-    it 'nameが空文字だと無効であること' do
-      department.name = ''
-      expect(department).to be_invalid
-      expect(department.errors[:name]).to include('部署名が空白です。')
+    describe '存在性の検証' do
+      it 'nameが空文字だと無効であること' do
+        department.name = ''
+        department.valid?
+        expect(department.errors.details[:name]).to include(error: :blank)
+      end
     end
-  end
 
-  describe '一意性の検証' do
-    it 'nameが重複した場合、無効であること' do
-      department.save
-      duplicate = FactoryBot.build(:department, name: department.name)
-      expect(duplicate).to be_invalid
-      expect(duplicate.errors[:name]).to include('部署名が重複しています。')
+    describe '一意性の検証' do
+      it 'nameが重複した場合、無効であること' do
+        department.save
+        duplicate = FactoryBot.build(:department, name: department.name)
+        duplicate.valid?
+        expect(duplicate.errors.details[:name].first).to include(error: :taken)
+      end
     end
   end
 end
