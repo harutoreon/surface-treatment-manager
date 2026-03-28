0d7dfd096d73a36c952613fc2c2008713f6d1bfa
reonharuto <norabder567@gmail.com>
2026-03-28 17:32:49 +0900

    Refactor searches controller and spec

--- backend/spec/requests/searches_spec.rb
+++ backend/spec/requests/searches_spec.rb
@@ -1,78 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe "Searches", type: :request do
-  before do
-    FactoryBot.create(:maker)
-    FactoryBot.create(:category)
-    FactoryBot.create_list(:sample_list, 5)
-  end
+  let!(:sample) { FactoryBot.create(:sample) }
 
   describe '#name_search' do
     it 'レスポンスのステータスがokであること' do
-      get '/name_search', params: { keyword: 'めっき' }
+      get name_search_path, params: { keyword: 'めっき' }
       expect(response).to have_http_status(:ok)
     end
 
-    it 'レスポンスのjsonに:samplesと:keywordが含まれていること' do
-      get '/name_search', params: { keyword: 'めっき' }
-      json = JSON.parse(response.body, symbolize_names: true)
-      expect(json.include?(:samples)).to be(true)
-      expect(json.include?(:keyword)).to be(true)
-      expect(json[:samples].count).to eq(5)
+    it 'レスポンスにsamplesとkeywordが含まれていること' do
+      get name_search_path, params: { keyword: 'めっき' }
+      json = response.parsed_body
+      expect(json[:samples].size).to eq(1)
       expect(json[:keyword]).to eq('めっき')
     end
   end
 
   describe '#category_search' do
     it 'レスポンスのステータスがokであること' do
-      get '/category_search', params: { keyword: 'めっき' }
+      get category_search_path, params: { keyword: 'めっき' }
       expect(response).to have_http_status(:ok)
     end
 
-    it 'レスポンスのjsonに:samplesと:keywordが含まれていること' do
-      get '/category_search', params: { keyword: 'めっき' }
-      json = JSON.parse(response.body, symbolize_names: true)
-
-      expect(json.include?(:samples)).to be(true)
-      expect(json.include?(:keyword)).to be(true)
-      expect(json[:samples].count).to eq(5)
+    it 'レスポンスにsamplesとkeywordが含まれていること' do
+      get category_search_path, params: { keyword: 'めっき' }
+      json = response.parsed_body
+      expect(json[:samples].size).to eq(1)
       expect(json[:keyword]).to eq('めっき')
     end
   end
 
   describe '#maker_search' do
     it 'レスポンスのステータスがokであること' do
-      get '/maker_search', params: { keyword: '松本情報合名会社' }
+      get maker_search_path, params: { keyword: '松本情報合名会社' }
       expect(response).to have_http_status(:ok)
     end
 
-    it 'レスポンスのjsonに:samplesと:keywordが含まれていること' do
-      get '/maker_search', params: { keyword: '松本情報合名会社' }
+    it 'レスポンスにsamplesとkeywordが含まれていること' do
+      get maker_search_path, params: { keyword: '松本情報合名会社' }
       json = response.parsed_body
-
-      expect(json.include?('samples')).to be(true)
-      expect(json.include?('keyword')).to be(true)
-      expect(json['samples'].count).to eq(5)
-      expect(json['keyword']).to eq('松本情報合名会社')
+      expect(json[:samples].size).to eq(1)
+      expect(json[:keyword]).to eq('松本情報合名会社')
     end
   end
 
   describe '#list_search' do
     it 'レスポンスのステータスがokであること' do
-      get '/list_search'
+      get list_search_path
       expect(response).to have_http_status(:ok)
     end
 
-    it 'jsonにサンプルが5件含まれていること' do
-      get '/list_search'
-      json = response.parsed_body
-      expect(json.count).to eq(5)
-    end
+    it 'レスポンスのレコードに過不足なく属性が含まれていること' do
+      get list_search_path
+      expect(response.parsed_body.size).to eq(1)
 
-    it 'jsonにid/name/summary/image_urlの属性が含まれていること' do
-      get '/list_search'
-      json = response.parsed_body
-      expect(json.first.keys).to match_array(%w(id name summary image_url))
+      expect(
+        response.parsed_body.first.keys
+      ).to match_array(%w(id name summary image_url))
     end
   end
 end
