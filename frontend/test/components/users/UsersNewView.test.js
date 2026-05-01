5ccaeac6315e227579c7012c7dd5ae724ab157e7
reonharuto <norabder567@gmail.com>
2026-05-02 06:04:28 +0900

    Modify UsersNewView.test.js

--- frontend/test/components/users/UsersNewView.test.js
+++ frontend/test/components/users/UsersNewView.test.js
@@ -24,274 +24,135 @@ vi.mock('vue-router', () => {
 })
 
 describe('UsersNewView', () => {
-  let wrapper
+  const mockResponse = [
+    { id: 1, name: '品質管理部' },
+  ]
 
-  describe('ログインチェックに成功した場合', () => {
-    it('ユーザー情報の登録ページに移動すること', async () => {
-      axios.get
-        .mockResolvedValueOnce({
-          status: 200
-        })
-        .mockResolvedValueOnce({
-          status: 200
-        })
-
-      wrapper = mount(UsersNewView, {
-        global: {
-          stubs: {
-            RouterLink: RouterLinkStub
-          }
-        }
-      })
-
-      await flushPromises()
-
-      expect(wrapper.find('h3').text()).toBe('ユーザー情報の登録')
-    })
+  const mountComponent = () => mount(UsersNewView, {
+    global: {
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    }
   })
 
-  describe('ログインチェックに失敗した場合', () => {
-    it('ログインページに移動すること', async () => {
-      axios.get.mockRejectedValue({
-        response: {
-          status: 401
-        }
-      })
-
-      wrapper = mount(UsersNewView, {
-        global: {
-          stubs: {
-            RouterLink: RouterLinkStub
-          }
-        }
-      })
-
-      await flushPromises()
-
-      expect(wrapper.emitted()).toHaveProperty('message')
-      expect(wrapper.emitted().message[0]).toEqual([
-        { type: 'danger', text: 'ログインが必要です。' }
-      ])
-      expect(pushMock).toHaveBeenCalledWith('/')
-      expect(pushMock).not.toHaveBeenCalledWith('/departments')
-    })
+  beforeEach(() => {
+    vi.clearAllMocks()
   })
 
   describe('初期レンダリング', () => {
-    beforeEach(async () => {
-      axios.get
-        .mockResolvedValueOnce({
-          status: 200
-        })
-        .mockResolvedValueOnce({
-          data: [
-            { id: 1, name: '品質管理部' },
-            { id: 2, name: '製造部' },
-            { id: 3, name: '開発部' },
-            { id: 4, name: '営業部' }
-          ]
-        })
+    describe('レンダリングに成功した場合', () => {
+      it('ユーザー情報の登録ページが表示されること', async () => {
+        vi.mocked(axios.get)
+          .mockResolvedValueOnce({ status: 200 })
+          .mockResolvedValueOnce({ data: mockResponse })
 
-      wrapper = mount(UsersNewView, {
-        global: {
-          stubs: {
-            RouterLink: RouterLinkStub
-          }
-        }
+        const wrapper = mountComponent()
+        await flushPromises()
+
+        // 見出し
+        expect(wrapper.find('h3').text()).toBe('ユーザー情報の登録')
+
+        // フォーム要素
+        expect(wrapper.find('form').exists()).toBe(true)
+
+        // ラベル要素
+        expect(wrapper.find('label[for="user-name"]').text()).toBe('ユーザー名')
+        expect(wrapper.find('label[for="user-department"]').text()).toBe('部署名')
+        expect(wrapper.find('label[for="user-password"]').text()).toBe('パスワード')
+        expect(wrapper.find('label[for="user-password-confirmation"]').text()).toBe('パスワードの確認')
+
+        // 入力要素
+        expect(wrapper.find('#user-name').exists()).toBe(true)
+        expect(wrapper.find('#user-password').exists()).toBe(true)
+        expect(wrapper.find('#user-password-confirmation').exists()).toBe(true)
+
+        // 選択要素
+        expect(wrapper.find('#user-department').exists()).toBe(true)
+        expect(wrapper.find('option[value="品質管理部"]').exists()).toBe(true)
+
+        // ボタン要素
+        expect(wrapper.find('button').text()).toBe('登録')
+
+        // 外部リンク
+        const routerLink = wrapper.findComponent(RouterLinkStub)
+        expect(routerLink.props().to).toBe('/users')
+        expect(routerLink.text()).toBe('ユーザーリスト')
       })
-
-      await flushPromises()
     })
 
-    it('見出しが表示されること', () => {
-      expect(wrapper.find('h3').text()).toBe('ユーザー情報の登録')
-    })
+    describe('レンダリングに失敗した場合', () => {
+      it('ログインページに遷移すること', async () => {
+        vi.mocked(axios.get).mockRejectedValueOnce({ response: { status: 401 }  })
 
-    it('入力フォームが表示されること', () => {
-      // フォーム要素
-      expect(wrapper.find('form').exists()).toBe(true)
+        const wrapper =  mountComponent()
+        await flushPromises()
 
-      // ラベル要素
-      expect(wrapper.find('label[for="user-name"]').text()).toBe('ユーザー名')
-      expect(wrapper.find('label[for="user-department"]').text()).toBe('部署名')
-      expect(wrapper.find('label[for="user-password"]').text()).toBe('パスワード')
-      expect(wrapper.find('label[for="user-password-confirmation"]').text()).toBe('パスワードの確認')
-
-      // 入力要素
-      expect(wrapper.find('#user-name').exists()).toBe(true)
-      expect(wrapper.find('#user-password').exists()).toBe(true)
-      expect(wrapper.find('#user-password-confirmation').exists()).toBe(true)
-
-      // 選択要素
-      expect(wrapper.find('#user-department').exists()).toBe(true)
-      expect(wrapper.find('option[value="品質管理部"]').exists()).toBe(true)
-      expect(wrapper.find('option[value="製造部"]').exists()).toBe(true)
-      expect(wrapper.find('option[value="開発部"]').exists()).toBe(true)
-      expect(wrapper.find('option[value="営業部"]').exists()).toBe(true)
-
-      // ボタン要素
-      expect(wrapper.find('button').text()).toBe('登録')
-    })
-
-    it('外部リンクが表示されること', () => {
-      const routerLink = wrapper.findComponent(RouterLinkStub)
-
-      expect(routerLink.props().to).toBe('/users')
-      expect(routerLink.text()).toBe('ユーザーリスト')
+        expect(wrapper.emitted('message')).toBeTruthy()
+        expect(wrapper.emitted('message')[0]).toEqual([
+          { type: 'danger', text: 'ログインが必要です。' }
+        ])
+        expect(pushMock).toHaveBeenCalledWith('/')
+      })
     })
   })
 
-  describe('部署名リストの取得に成功した場合', () => {
-    it('オプション要素に部署名がセットされること', async () => {
-      axios.get
-        .mockResolvedValueOnce({
-          status: 200
-        })
-        .mockResolvedValueOnce({
-          data: [
-            { id: 1, name: '品質管理部' },
-            { id: 2, name: '製造部' },
-            { id: 3, name: '開発部' },
-            { id: 4, name: '営業部' }
-          ]
-        })
 
-      wrapper = mount(UsersNewView, {
-        global: {
-          stubs: {
-            RouterLink: RouterLinkStub
+  describe('ユーザー登録', () => {
+    describe('有効な情報を送信した場合', () => {
+      it('登録に成功すること', async () => {
+        vi.mocked(axios.get)
+          .mockResolvedValueOnce({ status: 200 })
+          .mockResolvedValueOnce({ data: mockResponse })
+
+        vi.mocked(axios.post).mockResolvedValue({
+          data: {
+            id: 1,
+            name: '渡辺 陸斗',
+            department: '開発部',
+            password: 'password',
+            password_confirmation: 'password'
           }
-        }
+        })
+
+        const wrapper = mountComponent()
+        await flushPromises()
+
+        await wrapper.find('#user-name').setValue('渡辺 陸斗')
+        await wrapper.find('#user-department').setValue('開発部')
+        await wrapper.find('#user-password').setValue('password')
+        await wrapper.find('#user-password-confirmation').setValue('password')
+
+        await wrapper.find('form').trigger('submit')
+        await flushPromises()
+
+        expect(wrapper.emitted('message')).toBeTruthy()
+        expect(wrapper.emitted('message')[0]).toEqual([
+          { type: 'success', text: 'ユーザー情報を登録しました。' }
+        ])
+
+        expect(pushMock).toHaveBeenCalledWith('/users/1')
       })
-
-      await flushPromises()
-
-      expect(wrapper.find('option[value="品質管理部"]').exists()).toBe(true)
-      expect(wrapper.find('option[value="製造部"]').exists()).toBe(true)
-      expect(wrapper.find('option[value="開発部"]').exists()).toBe(true)
-      expect(wrapper.find('option[value="営業部"]').exists()).toBe(true)
     })
-  })
 
-  describe('部署名リストの取得に失敗した場合', () => {
-    it('404ページに遷移すること', async () => {
-      axios.get
-        .mockResolvedValueOnce({
-          status: 200
-        })
-        .mockRejectedValueOnce({
-          response: {
-            status: 404
-          }
-        })
+    describe('無効な情報を送信した場合', () => {
+      it('登録に失敗すること', async () => {
+        vi.mocked(axios.get)
+          .mockResolvedValueOnce({ status: 200 })
+          .mockResolvedValueOnce({ data: mockResponse })
 
-      wrapper = mount(UsersNewView, {
-        global: {
-          stubs: {
-            RouterLink: RouterLinkStub
-          }
-        }
+        vi.mocked(axios.post).mockRejectedValue({ response: { status: 422 } })
+
+        const wrapper = mountComponent()
+        await flushPromises()
+
+        await wrapper.find('#user-name').setValue('')
+
+        await wrapper.find('form').trigger('submit')
+        await flushPromises()
+
+        expect(wrapper.find('.alert').text()).toBe('入力に不備があります。')
       })
-
-      await flushPromises()
-
-      expect(wrapper.emitted()).toHaveProperty('message')
-      expect(wrapper.emitted().message[0]).toEqual([
-        { type: 'danger', text: '部署名の取得に失敗しました。' }
-      ])
-      expect(replaceMock).toHaveBeenCalledWith({ name: 'NotFound' })
-    })
-  })
-
-  describe('有効な情報を送信した場合', () => {
-    it('登録に成功すること', async () => {
-      axios.get
-        .mockResolvedValueOnce({
-          status: 200
-        })
-        .mockResolvedValueOnce({
-          data: [
-            { id: 1, name: '品質管理部' },
-            { id: 2, name: '製造部' },
-            { id: 3, name: '開発部' },
-            { id: 4, name: '営業部' }
-          ]
-        })
-
-      axios.post.mockResolvedValue({
-        data: {
-          id: 1,
-          name: '渡辺 陸斗',
-          department: '開発部',
-          password: 'password',
-          password_confirmation: 'password'
-        }
-      })
-
-      wrapper = mount(UsersNewView, {
-        global: {
-          stubs: {
-            RouterLink: RouterLinkStub
-          }
-        }
-      })
-
-      await flushPromises()
-
-      await wrapper.find('#user-name').setValue('渡辺 陸斗')
-      await wrapper.find('#user-department').setValue('開発部')
-      await wrapper.find('#user-password').setValue('password')
-      await wrapper.find('#user-password-confirmation').setValue('password')
-
-      await wrapper.find('form').trigger('submit.prevent')
-
-      expect(wrapper.emitted()).toHaveProperty('message')
-      expect(wrapper.emitted().message[0]).toEqual([
-        { type: 'success', text: 'ユーザー情報を登録しました。' }
-      ])
-
-      expect(pushMock).toHaveBeenCalledWith('/users/1')
-    })
-  })
-
-  describe('無効な情報を送信した場合', () => {
-    it('登録に失敗すること', async () => {
-      axios.get
-        .mockResolvedValueOnce({
-          status: 200
-        })
-        .mockResolvedValueOnce({
-          data: [
-            { id: 1, name: '品質管理部' },
-            { id: 2, name: '製造部' },
-            { id: 3, name: '開発部' },
-            { id: 4, name: '営業部' }
-          ]
-        })
-
-      axios.post.mockRejectedValue({
-        response: {
-          status: 422
-        }
-      })
-
-      wrapper = mount(UsersNewView, {
-        global: {
-          stubs: {
-            RouterLink: RouterLinkStub
-          }
-        }
-      })
-
-      await flushPromises()
-      await wrapper.find('#user-name').setValue('')
-      await wrapper.find('#user-department').setValue('開発部')
-      await wrapper.find('#user-password').setValue('password')
-      await wrapper.find('#user-password-confirmation').setValue('password')
-
-      await wrapper.find('form').trigger('submit.prevent')
-
-      expect(wrapper.text()).toContain('入力に不備があります。')
     })
   })
 })
