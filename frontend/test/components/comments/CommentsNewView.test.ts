c66819976dac3443ac9daa6221539bc99daae76f
reonharuto <norabder567@gmail.com>
2026-06-05 05:13:37 +0900

    Add linkking makers to samples test

--- frontend/test/components/comments/CommentsNewView.test.ts
+++ frontend/test/components/comments/CommentsNewView.test.ts
@@ -1,7 +1,7 @@
 import CommentsNewView from '@/components/comments/CommentsNewView.vue'
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
 import { mount, flushPromises, RouterLinkStub } from '@vue/test-utils'
-import type { Maker, User, Emit } from '@/composables/comments/useCommentsNew.ts'
+import type { Maker, User, SampleResponse, Emit } from '@/composables/comments/useCommentsNew.ts'
 import type { VueWrapper } from '@vue/test-utils'
 import { nextTick } from 'vue'
 import axios from 'axios'
@@ -41,6 +41,24 @@ describe('CommentsNewView', (): void => {
       admin: false,
     }
   ]
+
+  const mockSampleResponse: SampleResponse = {
+    samples: [
+      {
+        id: 1,
+        category_id: 1,
+        color: "イエローブラウンシルバー",
+        feature: "耐食性・耐摩耗性・耐薬品性・耐熱性",
+        film_thickness: "通常は3～5μm、厚めの場合は20～50μmまで可能",
+        hardness: "析出状態の皮膜硬度でHV550～HV700、熱処理後の皮膜硬度はHV950程度",
+        maker_id: 1,
+        name: "無電解ニッケルめっき",
+        summary: "電気を使わず化学反応で金属表面にニッケルを析出する技術です。",
+      },
+    ],
+    current_page: 1,
+    total_pages: 1
+  }
 
   const mountComponent = (): VueWrapper => mount(CommentsNewView, {
     global: {
@@ -154,7 +172,7 @@ describe('CommentsNewView', (): void => {
     })
 
     describe('投稿者の候補がない場合', (): void => {
-      it('投稿者名の一部を入力しても投稿者の候補が表示されないこと', async (): Promise<void> => {
+      it('投稿者名の一部を入力しても投稿者の候補が表示されず、部署名も表示されないこと', async (): Promise<void> => {
         vi.mocked(axios.get).mockResolvedValueOnce({ data: { status: 200 } })
         vi.mocked(axios.get).mockResolvedValueOnce({ data: mockMakerResponse })
         vi.mocked(axios.get).mockResolvedValueOnce({ data: [] })
@@ -167,20 +185,42 @@ describe('CommentsNewView', (): void => {
         await input.setValue(mockUserResponse[0].name.slice(0,2))  // 「岩崎 颯太」の一部の「岩崎」を入力
 
         expect(wrapper.find('form ul li').exists()).toBe(false)
+        expect((wrapper.find('#department').element as HTMLInputElement).value).toBe('')
       })
     })
   })
 
   describe('メーカーと表面処理の連動', (): void => {
     describe('メーカーの候補がある場合', (): void => {
-      it('メーカーが選択可能になり、選択後に表面処理のドロップダウンリストが表示されること', async (): Promise<void> => {
+      it('メーカーが選択可能になり、メーカーを選択後に表面処理の候補が表示されること', async (): Promise<void> => {
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: { status: 200 } })
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: mockMakerResponse })
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: mockUserResponse })
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: mockSampleResponse })
 
+        const wrapper: VueWrapper = mountComponent()
+        await flushPromises()
+
+        await wrapper.find('#makers').setValue(mockMakerResponse[0].name)
+
+        expect(wrapper.find('#samples').exists()).toBe(true)
+        expect(wrapper.findAll('#samples option')[1].text()).toBe(mockSampleResponse.samples[0].name)
       })
     })
 
     describe('メーカーの候補が無い場合', (): void => {
-      it('ドロップダウンリストのオプションがデフォルトのみであること', async (): Promise<void> => {
+      it('ドロップダウンリストのオプションがデフォルトのみで、表面処理のドロップダウンリストも表示されないこと', async (): Promise<void> => {
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: { status: 200 } })
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: [] })
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: mockUserResponse })
+        vi.mocked(axios.get).mockResolvedValueOnce({ data: mockSampleResponse })
 
+        const wrapper: VueWrapper = mountComponent()
+        await flushPromises()
+
+        expect(wrapper.findAll('#makers option').length).toBe(1)
+        expect(wrapper.findAll('#makers option')[0].text()).toBe('メーカーを選択して下さい')
+        expect(wrapper.find('#samples').exists()).toBe(false)
       })
     })
   })
