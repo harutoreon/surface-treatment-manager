a3f77f18622de1e27ef8a33caa503fba39a61e32
reonharuto <norabder567@gmail.com>
2026-06-07 19:07:05 +0900

    Add null guard and change exception handling

--- frontend/src/composables/comments/useCommentsDestroy.ts
+++ frontend/src/composables/comments/useCommentsDestroy.ts
@@ -5,27 +5,28 @@ import type { Comment, Emit } from '@/composables/comments/useCommentsShow.ts'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string
 
-export function useCommentsDestroy(
-  emit: Emit, makerId: Ref<number | null>, sampleId: Ref<number | null>, comment: Ref<Comment | null>) {
+export function useCommentsDestroy(emit: Emit, makerId: Ref<number | null>, sampleId: Ref<number | null>, comment: Ref<Comment | null>) {
   const router = useRouter()
 
-  const handleDelete = async () => {
+  const handleDelete = async (): Promise<void> => {
+    if (!makerId || !sampleId || !comment) return
+
     const confirmDelete = window.confirm('本当に削除しますか？')
     if (!confirmDelete) return
 
     try {
-      await axios.delete(`${API_BASE_URL}/makers/${makerId.value}/samples/${sampleId.value}/comments/${comment.value.id}`)
+      await axios.delete(
+        `${API_BASE_URL}/makers/${makerId.value}/samples/${sampleId.value}/comments/${comment.value.id}`
+      )
       emit('message', { type: 'success', text: 'コメント情報を1件削除しました。' })
       router.push('/comments')
     } catch (error) {
-      if (error.response && error.response.status === 404) {
+      if (axios.isAxiosError(error) && error.response?.status === 404) {
         emit('message', { type: 'danger', text: '削除処理に失敗しました。' })
         router.replace({ name: 'NotFound' })
       }
     }
   }
 
-  return {
-    handleDelete,
-  }
+  return { handleDelete }
 }
