9d5675ea216c57fc83173a37a770644df06f5139
reonharuto <norabder567@gmail.com>
2026-06-13 18:31:59 +0900

    Add category-name.spec.ts

--- frontend/tests/resource-search-flow/category-name.spec.ts
+++ frontend/tests/resource-search-flow/category-name.spec.ts
@@ -1,17 +1,23 @@
 import { test, expect } from '@playwright/test'
+import type { Page } from '@playwright/test'
+
+const fillLoginForm = async (page: Page, username: string, password: string): Promise<void> => {
+  await page.getByRole('textbox', { name: 'ユーザー名' }).fill(username)
+  await page.getByRole('textbox', { name: 'パスワード' }).fill(password)
+  await page.getByRole('button', { name: 'ログイン' }).click()
+}
 
 test.describe('カテゴリーで検索', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
-    await page.getByRole('radio', { name: '一般ユーザー' }).check()
-    await page.getByRole('button', { name: 'ログイン' }).click()
+    await fillLoginForm(page, 'general user', 'generalpassword')
     await page.getByRole('button', { name: '通知を閉じる' }).click()
 
     await page.locator('#search-category').getByRole('link', { name: '検索ページへ' }).click()
   })
 
-  test.describe('ドロップダウンリストで「めっき」を選び検索した場合', () => {
-    test('文字列「めっき」を含む表面処理がヒットすること', async ({ page }) => {
+  test.describe('カテゴリを選択して検索した場合', () => {
+    test('文字列を含む表面処理がヒットすること', async ({ page }) => {
       await page.getByRole('combobox').selectOption('めっき')
       await page.getByRole('button', { name: '検索' }).click()
 
