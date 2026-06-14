4f3b1fed18da0b18a7991250d2d5a15e6e7ca57c
reonharuto <norabder567@gmail.com>
2026-06-14 18:00:03 +0900

    Add makers.spec.ts

--- frontend/tests/resources-crud-flow/makers.spec.ts
+++ frontend/tests/resources-crud-flow/makers.spec.ts
@@ -1,17 +1,23 @@
 import { test, expect } from '@playwright/test'
+import type { Page } from '@playwright/test'
+
+const fillLoginForm = async (page: Page, username: string, password: string): Promise<void> => {
+  await page.getByRole('textbox', { name: 'ユーザー名' }).fill(username)
+  await page.getByRole('textbox', { name: 'パスワード' }).fill(password)
+  await page.getByRole('button', { name: 'ログイン' }).click()
+}
 
 test.describe('makers crud flow', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/')
-    await page.getByRole('radio', { name: '管理者ユーザー' }).check()
-    await page.getByRole('button', { name: 'ログイン' }).click()
+    await fillLoginForm(page, 'general user', 'generalpassword')
     await page.getByRole('button', { name: '通知を閉じる' }).click()
 
     await page.goto('/makers/new')
   })
 
-  test('メーカーの新規登録・更新・削除ができること', async ({ page }) => {
-    // /makers/newページの検証
+  test('メーカーの新規登録・参照・更新・削除ができること', async ({ page }) => {
+    // 登録ページへの遷移確認
     await expect(page.getByRole('heading', { name: 'メーカー情報の登録' })).toBeVisible()
 
     // メーカー情報の入力
@@ -27,9 +33,8 @@ test.describe('makers crud flow', () => {
     // 登録実行
     await page.getByRole('button', { name: '登録' }).click()
 
-    // /makers/idページの検証
+    // 詳細ページへの遷移確認
     await expect(page.getByRole('heading', { name: 'メーカー情報' })).toBeVisible()
-
     await expect(page.getByRole('listitem').filter({ hasText: '合資会社藤原印刷' })).toBeVisible()
     await expect(page.getByRole('listitem').filter({ hasText: '262-0044' })).toBeVisible()
     await expect(page.getByRole('listitem').filter({ hasText: '東京都渋谷区神南1-2-37' })).toBeVisible()
@@ -38,13 +43,12 @@ test.describe('makers crud flow', () => {
     await expect(page.getByRole('listitem').filter({ hasText: 'sample_maker67@example.com' })).toBeVisible()
     await expect(page.getByRole('listitem').filter({ hasText: 'https://example.com/sample_maker75' })).toBeVisible()
     await expect(page.getByRole('listitem').filter({ hasText: '阿部 蓮' })).toBeVisible()
-
     await page.getByRole('button', { name: '通知を閉じる' }).click()
 
     // メーカー情報の編集ページへ
     await page.getByRole('link', { name: 'メーカー情報の編集' }).click()
 
-    // /makers/id/editページの検証
+    // 更新ページへの遷移確認
     await expect(page.getByRole('heading', { name: 'メーカー情報の編集' })).toBeVisible()
     await expect(page.locator('#maker-name')).toHaveValue('合資会社藤原印刷')
     await expect(page.locator('#maker-postal-code')).toHaveValue('262-0044')
@@ -61,7 +65,7 @@ test.describe('makers crud flow', () => {
     // 更新を実行
     await page.getByRole('button', { name: '更新' }).click()
 
-    // /makers/idページの検証
+    // 詳細ページへの遷移確認
     await expect(page.getByRole('heading', { name: 'メーカー情報' })).toBeVisible()
 
     await expect(page.getByRole('listitem').filter({ hasText: '株式会社藤原印刷' })).toBeVisible()
@@ -79,9 +83,9 @@ test.describe('makers crud flow', () => {
     page.once('dialog', async dialog => {
       await dialog.accept()
     })
-    await page.locator('button', { name: 'メーカー情報の削除' }).click()
+    await page.locator('button', { hasText: 'メーカー情報の削除' }).click()
 
-    // /makersページの検証
+    // 一覧ページへの遷移確認
     await expect(page.getByRole('heading', { name: 'メーカーリスト' })).toBeVisible()
     await page.getByRole('button', { name: '通知を閉じる' }).click()
 
