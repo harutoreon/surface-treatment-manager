b939b6c4172c823ecf617a48ad108bbbadf9e01a
reonharuto <norabder567@gmail.com>
2026-08-23 17:11:39 +0900

    Add type to FilmThickness.test.js

--- frontend/test/components/static_pages/FilmThickness.test.ts
+++ frontend/test/components/static_pages/FilmThickness.test.ts
@@ -1,8 +1,13 @@
 import FilmThickness from '@/components/static_pages/FilmThickness.vue'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { RouterLinkStub, mount, flushPromises } from '@vue/test-utils'
+import type { VueWrapper } from '@vue/test-utils'
 
-const pushMock = vi.fn()
+const { pushMock } = vi.hoisted(() => {
+  return {
+    pushMock: vi.fn()
+  }
+})
 
 vi.mock('axios')
 vi.mock('vue-router', () => {
@@ -15,8 +20,8 @@ vi.mock('vue-router', () => {
   }
 })
 
-describe('FilmThickness Component', () => {
-  const mountComponent = () =>
+describe('FilmThickness Component', (): void => {
+  const mountComponent = (): VueWrapper =>
     mount(FilmThickness, {
       global: {
         stubs: {
@@ -25,20 +30,20 @@ describe('FilmThickness Component', () => {
       }
     })
 
-    beforeEach(() => {
+    beforeEach((): void => {
       vi.clearAllMocks()
     })
 
-  describe('初期レンダリングの検証', () => {
-    it('見出しが表示されること', async () => {
-      const wrapper = mountComponent()
+  describe('初期レンダリングの検証', (): void => {
+    it('見出しが表示されること', async (): Promise<void> => {
+      const wrapper: VueWrapper = mountComponent()
       await flushPromises()
 
       expect(wrapper.find('h3').text()).toBe('変寸量で検索')
     })
 
-    it('検索フォームが表示されること', async () => {
-      const wrapper = mountComponent()
+    it('検索フォームが表示されること', async (): Promise<void> => {
+      const wrapper: VueWrapper = mountComponent()
       await flushPromises()
 
       // フォーム要素
@@ -57,8 +62,8 @@ describe('FilmThickness Component', () => {
       expect(wrapper.find('button').text()).toBe('検索')
     })
 
-    it('ナビゲーションリンクが表示されること', async () => {
-      const wrapper = mountComponent()
+    it('ナビゲーションリンクが表示されること', async (): Promise<void> => {
+      const wrapper: VueWrapper = mountComponent()
       await flushPromises()
 
       const routerLink = wrapper.findComponent(RouterLinkStub)
@@ -67,16 +72,16 @@ describe('FilmThickness Component', () => {
     })
   })
 
-  describe('入力値の有効性検証', () => {
-    describe('膜厚と誤差を入力して検索した場合', () => {
-      it('検索結果ページに遷移すること', async () => {
-        const wrapper = mountComponent()
+  describe('入力値の有効性検証', (): void => {
+    describe('膜厚と誤差を入力して検索した場合', (): void => {
+      it('検索結果ページに遷移すること', async (): Promise<void> => {
+        const wrapper: VueWrapper = mountComponent()
         await flushPromises()
 
-        wrapper.find('#film-thickness').setValue('5')
-        wrapper.find('#allowable-error').setValue('2')
+        await wrapper.find('#film-thickness').setValue('5')
+        await wrapper.find('#allowable-error').setValue('2')
 
-        wrapper.find('button').trigger('submit')
+        await wrapper.find('form').trigger('submit')
         await flushPromises()
 
         expect(pushMock).toHaveBeenCalledWith(
@@ -91,15 +96,15 @@ describe('FilmThickness Component', () => {
       })
     })
 
-    describe('膜厚と誤差を未入力で検索した場合', () => {
-      it('バリデーションエラーになる', async () => {
-        const wrapper = mountComponent()
+    describe('膜厚と誤差を未入力で検索した場合', (): void => {
+      it('バリデーションエラーになる', async (): Promise<void> => {
+        const wrapper: VueWrapper = mountComponent()
         await flushPromises()
 
-        wrapper.find('#film-thickness').setValue('0')
-        wrapper.find('#allowable-error').setValue('0')
+        await wrapper.find('#film-thickness').setValue('0')
+        await wrapper.find('#allowable-error').setValue('0')
 
-        wrapper.find('button').trigger('submit')
+        await wrapper.find('form').trigger('submit')
         await flushPromises()
 
         expect(wrapper.find('.alert').text()).toBe('変寸量または誤差を入力して下さい。')
