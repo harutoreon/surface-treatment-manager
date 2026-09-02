ef3efb78a9f7d232f75cb3dc3451e9ffa425e830
reonharuto <norabder567@gmail.com>
2026-09-02 21:08:49 +0900

    Add useStaticPagesCategory.ts and useStaticPagesCategory.test.ts

---
+++ frontend/src/composables/static_pages/useStaticPagesCategory.ts
@@ -0,0 +1,52 @@
+import { ref } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+
+export type Option = {
+  id: number
+  item: string
+}
+export type Emit = (event: 'message', payload: { type: 'danger', text: string }) => void
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export function useStaticPagesCategory(emit: Emit) {
+  const router = useRouter()
+  const keyword = ref<string>('')
+  const options = ref<Option[]>([])
+  const errorMessage = ref<string>('')
+
+  const fetchCategories = async (): Promise<void> => {
+    try {
+      const response = await axios.get<Option[]>(`${API_BASE_URL}/categories`)
+      options.value = response.data
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 404) {
+        emit('message', { type: 'danger', text: 'カテゴリーの取得に失敗しました。' })
+        router.replace({ name: 'NotFound' })
+      }
+    }
+  }
+
+  const submitSearch = (): void => {
+    errorMessage.value = ''
+
+    if (!keyword.value) {
+      errorMessage.value = 'リスト内の項目を選択して下さい'
+      return
+    }
+
+    router.push({
+      name: 'SearchResults',
+      params: { searchMethod: 'category' },
+      query: { keyword: keyword.value }
+    })
+  }
+
+  return {
+    errorMessage,
+    keyword,
+    options,
+    fetchCategories,
+    submitSearch,
+  }
+}
